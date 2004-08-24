f7278f77fc7e5e2df2921c46fbb579396f760f7a
maege <maege@wellengang.localhost>
2004-08-24 10:15:35 +0200

    ChangeSet 1.30.1.20: "0" input bug solved src/state/fipi_offer_input.rb: "0" input bug solved

--- src/state/fipi_offer_input.rb
+++ src/state/fipi_offer_input.rb
@@ -19,8 +19,8 @@ module ODDB
 			end
 			def activation_charge_count
 				count = 0
-				count += 1 unless @fi_quantity==""
-				count += 1 unless @pi_quantity==""
+				count += 1 unless (@fi_quantity=="" || @fi_quantity=="0")
+				count += 1 unless (@pi_quantity=="" || @pi_quantity=="0")
 				count
 			end
 			def fi_charge
