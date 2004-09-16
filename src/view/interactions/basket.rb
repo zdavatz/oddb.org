c3d3d201b5e08e75a8d00a917bf8387257b3eb18
maege <maege@wellengang.localhost>
2004-09-16 22:38:32 +0200

    ChangeSet 1.30.1.57: corrected link in interaction basket src/view/interactions/basket.rb: corrected link in interaction basket

--- src/view/interactions/basket.rb
+++ src/view/interactions/basket.rb
@@ -25,11 +25,11 @@ class List < HtmlGrid::Component
 		context.ul {
 			@model.collect { |item| 
 				text = HtmlGrid::RichText.new(@model, @session, self)
-				pub_med_link = HtmlGrid::Link.new(:pub_med_link, @model, @session, self)
-				pub_med_link.href = @lookandfeel.lookup(:pub_med_href)
-				pub_med_link.value = @lookandfeel.lookup(:pub_med)
-				pub_med_link.target = "_blank"
-				text << pub_med_link	
+				pub_med_search_link = HtmlGrid::Link.new(:pub_med_search_link, @model, @session, self)
+				pub_med_search_link.href = @lookandfeel.lookup(:pub_med_search_href, item.substance_name)
+				pub_med_search_link.value = item.substance_name
+				pub_med_search_link.target = "_blank"
+				text << pub_med_search_link
 				item.links.each { |link|
 					alink = HtmlGrid::Link.new(:abstract_link, @model, @session, self)
 					alink.href = link.href
