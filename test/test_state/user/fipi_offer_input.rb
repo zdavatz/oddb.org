619f77ac24de6f994214eb93804d83f22a561264
xomex <xomex@oddb.org.localhost>
2004-09-16 09:56:34 +0200

    ChangeSet 1.30.1.55: added some forgotten files

---
+++ test/test_state/user/fipi_offer_input.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+# State::User::TestFiPiOfferInput -- oddb -- 09.09.2004 -- jlang@ywesee.com
+
+$: << File.expand_path('..', File.dirname(__FILE__))
+$: << File.expand_path("../../../src", File.dirname(__FILE__))
+
+require 'test/unit'
+require 'state/user/fipi_offer_input'
+
+module ODDB
+	module State
+		module User
+class TestFiPiOffer < Test::Unit::TestCase
+	def setup
+		@offer = FiPiOfferInput::FiPiOffer.new
+	end
+	def test_fi_quantity
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = 2
+		assert_equal(2, @offer.fi_quantity)
+		@offer.fi_quantity = "3"
+		assert_equal(3, @offer.fi_quantity)
+		@offer.fi_quantity = ""
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = "0"
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = 0
+		assert_equal(0, @offer.fi_quantity)
+	end
+	def test_pi_quantity
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = 2
+		assert_equal(2, @offer.fi_quantity)
+		@offer.fi_quantity = "3"
+		assert_equal(3, @offer.fi_quantity)
+		@offer.fi_quantity = ""
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = "0"
+		assert_equal(0, @offer.fi_quantity)
+		@offer.fi_quantity = 0
+		assert_equal(0, @offer.fi_quantity)
+	end
+	def test_fi_update_charge
+		assert_equal(0, @offer.fi_update_charge)
+		@offer.fi_update = 'update_ywesee'
+		assert_equal(@offer.class::FI_UPDATE, @offer.fi_update_charge)
+		@offer.fi_update = 'update_adsf'
+		assert_equal(0, @offer.fi_update_charge)
+	end
+	def test_pi_update_charge
+		assert_equal(0, @offer.fi_update_charge)
+		@offer.fi_update = 'update_ywesee'
+		assert_equal(@offer.class::FI_UPDATE, @offer.fi_update_charge)
+		@offer.fi_update = 'update_adsf'
+		assert_equal(0, @offer.fi_update_charge)
+	end
+	def test_calculate_activation_charge
+		assert_equal(2500, @offer.calculate_activation_charge)
+	end
+	def test_calculate_fi_update
+		assert_equal(0, @offer.calculate_fi_update)
+		@offer.fi_quantity = 1
+		@offer.fi_update = 'update_ywesee'
+		assert_equal(150, @offer.calculate_fi_update)
+		@offer.fi_quantity = 3
+		@offer.fi_update = 'update_ywesee'
+		assert_equal(450, @offer.calculate_fi_update)
+	end
+	def test_calculate_pi_update
+		assert_equal(0, @offer.calculate_pi_update)
+		@offer.pi_quantity = 1
+		@offer.pi_update = 'update_ywesee'
+		assert_equal(90, @offer.calculate_pi_update)
+		@offer.pi_quantity = 3
+		@offer.pi_update = 'update_ywesee'
+		assert_equal(270, @offer.calculate_pi_update)
+	end
+end
+		end
+	end
+end
