75e1431ac3b435bdeabf46473683ba84f8698630
hwyss <hwyss@oldie.ywesee.local>
2004-09-27 16:07:38 +0200

    ChangeSet 1.45: .del-db_connection.rb~8081320826ee0af6: ChangeSet 1.45:   Delete: etc/db_connection.rb etc/db_connection.rb: Delete: etc/db_connection.rb

--- etc/db_connection.rb
+++
@@ -1,7 +0,0 @@
-require 'dbi'
-
-@dbi = DBI.connect('DBI:pg:odba', 'odbauser', 'test')
-#@dbi = DBI.connect('DBI:mysql:odba', 'odbauser', 'test')
-#puts @dbi.handle.class
-#ODBA.storage.DB_TYPE = "mysql"
-ODBA.storage.dbi = @dbi
