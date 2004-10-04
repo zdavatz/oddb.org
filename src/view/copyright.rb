bfd8b2da118ce7ec530ae702c8ac6d1a002a956d
hwyss <hwyss@oldie.ywesee.local>
2004-10-04 16:39:22 +0200

    ChangeSet 1.67: link version number to bitkeeper repository src/view/copyright.rb: link version number to bitkeeper repository

--- src/view/copyright.rb
+++ src/view/copyright.rb
@@ -10,15 +10,24 @@ module ODDB
 		class Copyright < HtmlGrid::Composite
 			COMPONENTS = {
 				[0,0]			=>	:cpr_link,
-				[0,0,0]		=>	"oddb_version",
+				[0,0,0]		=>	:oddb_version,
 			}
 			CSS_CLASS = "navigation"
 			HTML_ATTRIBUTES = {"align"=>"left"}
 			SYMBOL_MAP = {
 				:cpr_link			=>	HtmlGrid::Link,
 			}
+			LEGACY_INTERFACE = false
+=begin
 			def cpr_date(model, session)
 				HtmlGrid::DateValue.new(:last_update, session.app, session, self)
+			end
+=end
+			def oddb_version(model)
+				link = HtmlGrid::Link.new(:oddb_version, model, @session, self)
+				link.href = 'http://ywesee.bkbits.net:8080/oddb.org'
+				link.css_class = 'navigation'
+				link
 			end
 		end
 	end
