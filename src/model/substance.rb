8648ce8da2c8c5cd420ba6125962bfa0a1ed6ab6
hwyss <hwyss@oldie.ywesee.local>
2004-10-07 07:54:05 +0200

    ChangeSet 1.100.1.1: optimize Substance#merge method src/model/substance.rb: optimize merge method

--- src/model/substance.rb
+++ src/model/substance.rb
@@ -113,10 +113,14 @@ module ODDB
 			end
 		end
 		def merge(other)
-			other.sequences.dup.each { |sequence|
+			other.sequences.uniq.each { |sequence|
 				if(active_agent = sequence.active_agent(other))
-					active_agent.substance = self
-					active_agent.odba_isolated_store
+					if(active_agent.sequence.nil?)
+						active_agent.odba_delete
+					else
+						active_agent.substance = self
+						active_agent.odba_isolated_store
+					end
 				else
 					puts "only removing sequence"
 					other.remove_sequence(sequence)
