92c49eed82b7904f49ed077c2cab7586382e4ab6
neo <neo@neo.(none)>
2004-11-26 11:44:53 +0100

    bkcg: autocommit

--- src/view/user/download_export.rb
+++ src/view/user/download_export.rb
@@ -15,9 +15,6 @@ class DownloadExportComposite < HtmlGrid::Composite
 	COMPONENTS = {
 		[0,0]		=>	'download_export_title',
 		[0,1]		=>	:download_export_descr,
-		[0,2]		=>	'download_export_feedback',
-		[0,3]		=>  'yaml_link_descr',
-		[0,3,1] =>	:yaml_link,
 		[0,4]		=>	View::User::YamlExportInnerComposite,
 		[0,5]		=>	View::User::OddbDatDownloadInnerComposite,
 		[0,6]		=>	View::User::FachinfoPDFDownloadInnerComposite,
