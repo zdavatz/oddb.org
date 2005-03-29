ae8e9d9b2077a5a1ba54bba4218ef4fd76155cee
xomex <xomex@oddb.org.localhost>
2005-03-29 13:48:14 +0200

    ChangeSet 1.255.1.24: added hospitals on root panel src/state/admin/root.rb: added hospitals on root panel

--- src/state/admin/root.rb
+++ src/state/admin/root.rb
@@ -170,7 +170,7 @@ module Root
 		State::Substances::EffectiveSubstances.new(@session, model)
 	end
 	def zones
-		[:drugs, :interactions, :substances, :companies, :user, :doctors, :admin]
+		[:drugs, :interactions, :substances, :companies, :doctors, :hospitals, :user, :admin]
 	end
 end
 		end
