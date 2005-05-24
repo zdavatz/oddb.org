c1cb0dce4fd625df5989ccc91119495d1b9dde0f
ywesee <ywesee@whitie.ywesee.com>
2005-05-24 15:08:04 +0200

    ChangeSet 1.335.1.2: global.rb: ChangeSet 1.335.1.2:   minor bugfix ChangeSet 1.335.1.2: oddb.rb: ChangeSet 1.335.1.2:   update to work with newest sbsm-session ChangeSet 1.335.1.2: pointersteps.rb: ChangeSet 1.335.1.2:   bugfix ChangeSet 1.335.1.2: fachinfo.rb: ChangeSet 1.335.1.2:   disable news from gone url src/plugin/fachinfo.rb: disable news from gone url src/view/pointersteps.rb: bugfix src/util/oddb.rb: update to work with newest sbsm-session src/state/global.rb: minor bugfix

--- src/util/oddb.rb
+++ src/util/oddb.rb
@@ -8,37 +8,6 @@ require 'drb/drb'
 require 'util/oddbapp'
 require 'etc/db_connection'
 
-while arg = ARGV.shift
-  case arg
-  when '--detach'
-    $detach = true
-  when /^--pidfile=(.+)/
-    $pidfile = $1
-  else
-    STDERR.puts "usage: #$0 [--detach] [--pidfile=PIDFILE] [--env]"
-    exit 1
-  end
-end
-
-if $pidfile
-	if pidfile = open($pidfile, "w")
-		pidfile.puts $$
-		pidfile.close
-		
-		END { File.unlink $pidfile }
-	end
-end
-
-if $detach
-	puts "detaching"
-  Process.fork and exit!(0)
-  Process.setsid
-  Process.fork and exit!(0)
-  
-  trap("INT") { }
-end
-
-#trap("HUP") { puts "caught HUP signal, shutting down\n"; exit }
 trap("USR1") { 
 	puts "caught USR1 signal, clearing Sessions\n"
 	$oddb.clear 
@@ -47,17 +16,18 @@ trap("USR2") {
 	puts "caught USR2 signal, flushing stdout...\n"
 	$stdout.flush
 }
-#trap("TERM") { puts "caught TERM signal, exiting immediately\n"; exit }
 
 $oddb = ODDB::App.new
-
-#require 'profile'
 
 $0 = "Oddb (OddbApp)"
 
 DRb.start_service(ODDB::SERVER_URI, $oddb)
-#puts "drb-service started in #{seconds} seconds"
 
+start = Time.now
+puts "prefetching..."
+$stdout.flush
 ODBA.cache_server.prefetch
+puts "prefetch completed in #{Time.now-start} seconds"
+$stdout.flush
 
 DRb.thread.join
