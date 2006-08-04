a530335dccae8d4293b4ed56edf316ed7fa26d63
Hannes Wyss <hwyss@ywesee.com>
2006-08-04 14:29:30 +0200

    Disable 0-9 in MiGel

--- src/util/interval.rb
+++ src/util/interval.rb
@@ -16,10 +16,14 @@ module ODDB
 		}
 		FILTER_THRESHOLD = 30
 		attr_reader :range
+		def default_interval
+			intervals.first || 'a-d'
+		end
 		def filter_interval
 			if(@model.size > self::class::FILTER_THRESHOLD)
-				@range = self::class::RANGE_PATTERNS.fetch(user_range) {
-					self::class::RANGE_PATTERNS[default_interval]
+        ptrns = range_patterns
+				@range = ptrns.fetch(user_range) {
+					ptrns[default_interval]
 				}
 				pattern = if(@range=='|unknown')
 					/^($|[^a-zäÄáÁâÂàÀçÇëËéÉêÊèÈïÏíÍîÎìÌöÖóÓôÔòÒüÜúÚûÛùÙ])/i
@@ -35,29 +39,29 @@ module ODDB
 				}
 			end
 		end
-		def default_interval
-			intervals.first || 'a-d'
-		end
 		def get_intervals
 			@model.collect { |item| 
-				self::class::RANGE_PATTERNS.collect { |range, pattern| 
+				range_patterns.collect { |range, pattern| 
 					range if /^[#{pattern}]/i.match(item.send(*symbol))
 				}.compact.first || '|unknown'
 			}.flatten.uniq.sort
 		end
 		def interval
-			@interval ||= self::class::RANGE_PATTERNS.index(@range)
+			@interval ||= range_patterns.index(@range)
 		end
 		def intervals
 			@intervals ||= get_intervals
 		end
+    def range_patterns
+      self.class.const_get(:RANGE_PATTERNS)
+    end
 		def user_range
 			range = if(self::class::PERSISTENT_RANGE)
 				@session.persistent_user_input(:range)
 			else
 				@session.user_input(:range)
 			end
-			unless(self.class.const_get(:RANGE_PATTERNS).include?(range))
+			unless(range_patterns.include?(range))
 				range = default_interval
 			end
 			range
