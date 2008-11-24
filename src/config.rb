cfa6d843da53695600fabf0531ee936397f84717
Hannes Wyss <hwyss@ywesee.com>
2008-11-24 09:13:19 +0100

    Fix Config-path (for BSV-Plugin)

--- src/config.rb
+++ src/config.rb
@@ -4,7 +4,7 @@
 require 'rclconf'
 
 module ODDB
-  oddb_dir = File.expand_path('..', ENV['DOCUMENT_ROOT'] || './doc')
+  oddb_dir = File.expand_path('..', File.dirname(__FILE__))
   default_dir = File.expand_path('etc', oddb_dir)
   default_config_files = [
     File.join(default_dir, 'oddb.yml'),
@@ -12,6 +12,7 @@ module ODDB
   ]
   defaults = {
     'config'			        => default_config_files,
+    'url_bag_sl_zip'      => 'http://bag.e-mediat.net/SL2007.Web.External/File.axd?file=XMLPublications.zip',
   }
 
   config = RCLConf::RCLConf.new(ARGV, defaults)
