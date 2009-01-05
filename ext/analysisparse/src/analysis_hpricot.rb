c4f3370082ab0c466a1a2ad2ea3ef747ded8af34
Hannes Wyss <hwyss@ywesee.com>
2009-01-05 17:25:49 +0100

    Adapt Analysis-Parser to newes Hpricot

--- ext/analysisparse/src/analysis_hpricot.rb
+++ ext/analysisparse/src/analysis_hpricot.rb
@@ -18,7 +18,7 @@ module ODDB
 			def fetch_position_hrefs(doc, &block) 
 				(doc/"a").each { |pos| 
 					code = pos.inner_html
-					url = URL_BASE + pos.at("a")['href']
+					url = URL_BASE + pos.attributes['href']
 					info = fetch_additional_info(open(url))
 					block.call(code, info)
 				}
@@ -28,32 +28,31 @@ module ODDB
 				doc = Hpricot(html)
 				elements = doc.search("//tr[@align='left']")
 				elements = (elements/"//td[@colspan='2']")
-				(0..(elements.size-1)).each { |index|
-					elem = elements.at(index)
+        elements.each_with_index { |elem, index|
 					case (elem/"strong").inner_html
 					when "Beschreibung"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_description, val)
 					when "Interpretation"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_interpretation, val)
 					when "Indikation"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_indication, val)
 					when "Aussagekraft (Bewertung)"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_significance, val)
 					when "Entnahmematerial"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 							result.store(:info_ext_material, val)
 					when "Entnahmebedingungen"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_ext_condition, val)
 					when "Lagerungsbedingungen"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_storage_condition, val)
 					when "Lagerunsgdauer"
-						val = format_string(elements.at(index+1).inner_html)
+						val = format_string(elements[index+1].inner_html)
 						result.store(:info_storage_time, val)
 					end
 				}
