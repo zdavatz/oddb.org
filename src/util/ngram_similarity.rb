c3ed4896c64961156bd0fb3f1518ba4078de5078
Hannes Wyss <hwyss@ywesee.com>
2009-06-19 12:14:35 +0200

    Added an NGram-Similiarity calculator
    This can be used in #company_by_name by supplying a cutoff-point
    (which should be a Float between 0.0 and 1.0)

---
+++ src/util/ngram_similarity.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# n-gram similarity -- oddb.org -- 19.06.2009 -- hwyss@ywesee.com
+
+module ODDB
+  module Util
+module NGramSimilarity
+  def self.compare(str1, str2, n=5)
+    str1 = u(str1).downcase.gsub(/[\s,.\-\/]+/, '')
+    str2 = u(str2).downcase.gsub(/[\s,.\-\/]+/, '')
+    if(str1.length < str2.length)
+      str1, str2 = str2, str1
+    end
+    parts = [ str1.length - n, 0 ].max + 1
+    count = 0
+    parts.times { |idx|
+      if(str2.include? str1[idx, n])
+        count += 1
+      end
+    }
+    count.to_f / parts
+  end
+end
+  end
+end
