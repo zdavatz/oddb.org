zdavatz/oddb.org

commit 00236415a5db595bd585866ca3e87a463e5aa503

Correct the name used before invoicing, in case it has changed
This implements http://trac.ywesee.com/ticket/151

src/plugin/info_invoicer.rb
@@ -155,6 +155,8 @@ module ODDB
       items.each { |item| 
         ptr = item.item_pointer
         if(seq = pointer_resolved(ptr))
+          ## correct the item's stored name (it may have changed in Packungen.xls)
+          item.data[:name] = seq.name if seq.respond_to?(:name)
           (companies[seq.company.odba_instance] ||= []).push(item)
         end
       }

commit 59bab3be8136f00f5c5ea64909c48589c701ad56

Fix a Bug introduced in 00236415a5db595bd585866ca3e87a463e5aa503
where invoice items without pre-existing @data crashed the invoicing
process

src/plugin/info_invoicer.rb
@@ -156,7 +156,7 @@ module ODDB
         ptr = item.item_pointer
         if(seq = pointer_resolved(ptr))
           ## correct the item's stored name (it may have changed in Packungen.xls)
-          item.data[:name] = seq.name if seq.respond_to?(:name)
+          (item.data ||= {})[:name] = seq.name if seq.respond_to?(:name)
           (companies[seq.company.odba_instance] ||= []).push(item)
         end
       }