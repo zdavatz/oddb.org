ee3e91bf9db191a9f458c0f0cb0b11818822de8b
Hannes Wyss <hwyss@ywesee.com>
2010-01-27 16:47:31 +0100

    Smaller version of the Facebook-Widget

--- src/view/drugs/centeredsearchform.rb
+++ src/view/drugs/centeredsearchform.rb
@@ -132,11 +132,11 @@ class CenteredSearchComposite < View::CenteredSearchComposite
 	end
   def facebook(model, session=@session)
     <<-EOS
-<script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/en_US"></script><script type="text/javascript">
-  FB.init("3252fd8899d16ddc5b75a43ecfcf3e8b");
-</script>
-<fb:fan profile_id="273461529221" stream="0" connections="10" logobar="1" width="300"></fb:fan>
-<div style="font-size:8px; padding-left:10px"><a href="http://www.facebook.com/pages/Generika/273461529221">Generika</a> on Facebook</div>
+<script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/en_US"></script>
+<script type="text/javascript">FB.init("3252fd8899d16ddc5b75a43ecfcf3e8b");</script>
+<fb:fan profile_id="273461529221" stream="0" connections="0" logobar="1" width="300"></fb:fan>
+<div style="font-size:8px; padding-left:10px">
+<a href="http://www.facebook.com/pages/Generika/273461529221">Generika</a> on Facebook</div>
     EOS
   end
 	def fi_count_text(model, session)
