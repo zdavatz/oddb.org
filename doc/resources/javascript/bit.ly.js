b61a4e543c33b5eedfb7b66b1d39b7543f889ff5
Hannes Wyss <hwyss@ywesee.com>
2010-02-01 11:07:18 +0100

    Fix bit.ly xhr calls.

--- doc/resources/javascript/bit.ly.js
+++ doc/resources/javascript/bit.ly.js
@@ -1,21 +1,24 @@
+var bitly_callback;
 function bitly_for_twitter(url, tweet)
 {
   document.body.style.cursor = 'wait';
-  var bitly = "http://api.bit.ly/shorten?version=2.0.1&longUrl=";
+  var bitly = "http://api.bit.ly/shorten?callback=bitly_callback&version=2.0.1&longUrl=";
   bitly += url + "&login=oddb&apiKey=R_0e2f8d420324c59276c450b5253b4e3f";
-  dojo.xhrGet({
+  bitly_callback = function(data) {
+    var result;
+    var redirect;
+    document.body.style.cursor = 'auto';
+    if(data.errorCode == '0') {
+      result = data.results[unescape(url)];
+      redirect = result.shortUrl;
+      document.location.href = tweet + redirect;
+    } else {
+      alert(data.errorMessage);
+    }
+  };
+  dojo.io.script.get({
 		url: bitly,
-    handleAs: 'json',
-		load: function(data, ioargs) {
-      var redirect;
-      document.body.style.cursor = 'auto';
-      if(data['errorCode'] == '0') {
-        redirect = data['results']['shortUrl'];
-        document.location.href = tweet + redirect;
-      } else {
-        alert(data['errorMessage']);
-      }
-		},
+    checkString: 'bitly_callback',
     error: function(error, ioargs) {
       console.debug(error);
       console.debug(ioargs);
