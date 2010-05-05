34e07f251ce52cb9167a6397ad655d3b76bed71e
Hannes Wyss <hwyss@ywesee.com>
2010-05-05 12:03:42 +0200

    Added missing file text_info.rb

---
+++ src/plugin/text_info.rb
@@ -0,0 +1,294 @@
+require 'config'
+require 'plugin/plugin'
+require 'drb'
+require 'mechanize'
+require 'model/fachinfo'
+require 'model/patinfo'
+
+module ODDB
+  class TextInfoPlugin < Plugin
+    def initialize *args
+      super
+      @parser = DRb::DRbObject.new nil, FIPARSE_URI
+      @dirs = {
+        :fachinfo => File.join(ODDB.config.data_dir, 'html', 'fachinfo'),
+        :patinfo  => File.join(ODDB.config.data_dir, 'html', 'patinfo'),
+      }
+      @updated_fis = 0
+      @updated_pis = 0
+      @ignored_pseudos = 0
+      @session_failures = 0
+      @up_to_date = Hash.new 0
+      @iksless = []
+      @unknown_iksnrs = {}
+      @failures = []
+      @download_errors = []
+    end
+    def init_agent
+      agent = Mechanize.new
+      agent.user_agent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_4_11; de-de) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.22"
+      agent
+    end
+    def init_searchform agent
+      url = ODDB.config.text_info_searchform \
+        or raise 'please configure ODDB.config.text_info_searchform to proceed'
+      page = agent.get(url)
+      form, = page.form_with :name => 'frmNutzungsbedingungen'
+      if form
+        if btn = form.button_with(:name => 'btnAkzeptieren')
+          page = agent.submit form, btn
+        end
+      end
+      page
+    end
+    def detect_session_failure page
+      !page.form_with(:name => 'frmSearchForm').nil?
+    end
+    def download_info type, name, agent, form, eventtarget
+      max_retries = ODDB.config.text_info_max_retry
+      paths = {}
+      de, fr = nil
+      begin
+        de = submit_event agent, form, eventtarget
+        if detect_session_failure(de)
+          @session_failures += 1
+          form = rebuild_resultlist agent
+          de = submit_event agent, form, eventtarget
+        end
+        if match = /(Pseudo-Fach|Produkt)information/i.match(de.body)
+          @ignored_pseudos += 1
+          return {}
+        end
+        if path = save_info(type, name, 'de', de)
+          paths.store :de, path
+        else
+          @up_to_date[type] += 1
+        end
+      rescue Mechanize::ResponseCodeError => err
+        retries ||= max_retries
+        if retries > 0
+          retries -= 1
+          sleep max_retries - retries
+          retry
+        else
+          raise
+        end
+      end
+      begin
+        fr = agent.get de.uri.to_s.gsub('lang=de', 'lang=fr')
+        if detect_session_failure(fr)
+          @session_failures += 1
+          form = rebuild_resultlist agent
+          de = submit_event agent, form, eventtarget
+          fr = agent.get de.uri.to_s.gsub('lang=de', 'lang=fr')
+        end
+        if path = save_info(type, name, 'fr', fr)
+          paths.store :fr, path
+        else
+          @up_to_date[type] += 1
+        end
+      rescue Mechanize::ResponseCodeError => err
+        retries ||= max_retries
+        if retries > 0
+          retries -= 1
+          sleep max_retries - retries
+          retry
+        else
+          raise
+        end
+      end
+      paths
+    rescue Mechanize::ResponseCodeError
+      @download_errors.push name
+      paths
+    end
+    def save_info type, name, lang, page
+      dir = File.join @dirs[type], lang
+      FileUtils.mkdir_p dir
+      tmp = File.join dir, name.gsub(/[\/\s\+:]/, '_') + '.tmp.html'
+      page.save tmp
+      path = File.join dir, name.gsub(/[\/\s\+:]/, '_') + '.html'
+      if File.exist?(path) && FileUtils.compare_file(tmp, path)
+        FileUtils.rm tmp
+        nil
+      else
+        FileUtils.mv tmp, path
+        path
+      end
+    end
+    def eventtarget string
+      if match = /doPostBack\('([^']+)'.*\)/.match(string.to_s)
+        match[1]
+      end
+    end
+    def extract_iksnrs languages
+      iksnrs = []
+      languages.each_value do |doc|
+        src = doc.iksnrs.to_s.gsub("'", "")
+        if(match = src.match(/[0-9]{3,5}(?:\s*,\s*[0-9]{3,5})*/u))
+          iksnrs.concat match.to_s.split(/\s*,\s*/u)
+        end
+      end
+      iksnrs.collect! do |iksnr| sprintf("%05i", iksnr.to_i) end
+      iksnrs.uniq!
+      iksnrs
+    rescue
+      []
+    end
+    def identify_eventtargets page, ptrn
+      eventtargets = {}
+      page.links_with(:href => ptrn).each do |link|
+        eventtargets.store link.text, eventtarget(link.href)
+      end
+      eventtargets
+    end
+    def import_products page, agent
+      fi_sources = identify_eventtargets page, /btnFachinformation/
+      pi_sources = identify_eventtargets page, /btnPatientenn?information/
+      form = page.form_with :name => 'frmResultProdukte'
+      fi_sources.sort.each do |name, eventtarget|
+        import_product name, agent, form, eventtarget, pi_sources[name]
+      end
+    end
+    def import_product name, agent, form, fi_target, pi_target
+      fi_paths = download_info :fachinfo, name, agent, form, fi_target
+      if pi_target
+        pi_paths = download_info :patinfo, name, agent, form, pi_target
+      end
+      update_product name, fi_paths, pi_paths || {}
+    end
+    def import_company name, agent=init_agent
+      @current_company = name
+      # search for company
+      page = search_company name, agent
+      # import each company from the result
+      import_companies page, agent
+    end
+    def import_companies page, agent
+      form = page.form_with :name => 'frmResulthForm'
+      page.links_with(:href => /Linkbutton1/).each do |link|
+        if et = eventtarget(link.href)
+          @current_eventtarget = et
+          products = submit_event agent, form, et
+          import_products products, agent
+        end
+      end
+    end
+    def parse_fachinfo path
+      @parser.parse_fachinfo_html path
+    end
+    def parse_patinfo path
+      @parser.parse_patinfo_html path
+    end
+    def rebuild_resultlist agent
+      page = search_company @current_company, agent
+      form = page.form_with :name => 'frmResulthForm'
+      products = submit_event agent, form, @current_eventtarget
+      products.form_with :name => 'frmResultProdukte'
+    end
+    def replace text_info, container, type
+      old_ti = container.send type
+      @app.update container.pointer, type => text_info.pointer
+      if old_ti && old_ti.empty?
+        @app.delete old_ti.pointer
+      end
+    end
+    def report
+      unknown_size = @unknown_iksnrs.size
+      unknown = @unknown_iksnrs.collect { |iksnr, name|
+        "#{name} (#{iksnr})"
+      }.join("\n")
+      [
+        "Stored #{@updated_fis} Fachinfos",
+        "Ignored #{@ignored_pseudos} Pseudo-Fachinfos",
+        "Ignored #{@up_to_date[:fachinfo]} up-to-date Fachinfo-Texts",
+        "Stored #{@updated_pis} Patinfos",
+        "Ignored #{@up_to_date[:patinfo]} up-to-date Patinfo-Texts", nil,
+        "Unknown Iks-Numbers: #{unknown_size}",
+        unknown, nil,
+        "Fachinfos without iksnrs: #{@iksless.size}",
+        @iksless.join("\n"), nil,
+        "Session failures: #{@session_failures}", nil,
+        "Download errors: #{@download_errors.size}",
+        @download_errors.join("\n"), nil,
+        "Parse Errors: #{@failures.size}", 
+        @failures.join("\n"), 
+      ].join("\n")
+    end
+    def search_company name, agent
+      page = init_searchform agent
+      form = page.form_with :name => 'frmSearchForm'
+      form.radiobutton_with(:value => 'rbFirma').click
+      form['txtSearch'] = name
+      agent.submit form
+    end
+    def store_fachinfo languages
+      @updated_fis += 1
+      pointer = Persistence::Pointer.new(:fachinfo)
+      @app.update(pointer.creator, languages)
+    end
+    def store_orphaned iksnr, fis, pis
+      pointer = Persistence::Pointer.new :orphaned_fachinfo
+      store = {
+        :key => iksnr,
+        :languages => fis,
+      }
+      @app.update pointer.creator, store
+      pointer = Persistence::Pointer.new :orphaned_patinfo
+      store = {
+        :key => iksnr,
+        :languages => pis,
+      }
+      @app.update pointer.creator, store
+    end
+    def store_patinfo reg, languages
+      @updated_pis +=1
+      existing = reg.sequences.collect do |seqnr, seq|
+        seq.patinfo end.compact.first
+      ptr = Persistence::Pointer.new(:patinfo).creator
+      if existing
+        ptr = existing.pointer
+      end
+      @app.update ptr, languages
+    end
+    def submit_event agent, form, eventtarget, *args
+      form['__EVENTTARGET'] = eventtarget
+      agent.submit form, *args
+    end
+    def update_product name, fi_paths, pi_paths
+      # parse pi and fi
+      fis = {}
+      fi_paths.each do |lang, path|
+        fis.store lang, parse_fachinfo(path)
+      end
+      pis = {}
+      pi_paths.each do |lang, path|
+        pis.store lang, parse_patinfo(path)
+      end
+      unless fis.empty?
+        # identify registration
+        iksnrs = extract_iksnrs fis
+        if iksnrs.empty?
+          @iksless.push name
+        end
+        fachinfo, patinfo = nil
+        # assign infos.
+        iksnrs.each do |iksnr|
+          if reg = @app.registration(iksnr)
+            fachinfo ||= store_fachinfo(fis)
+            replace fachinfo, reg, :fachinfo
+            patinfo ||= store_patinfo(reg, pis)
+            reg.each_sequence do |seq|
+              replace patinfo, seq, :patinfo
+            end
+          else
+            store_orphaned iksnr, fis, pis
+            @unknown_iksnrs.store iksnr, name
+          end
+        end
+      end
+    rescue RuntimeError => err
+      @failures.push err.message
+    end
+  end
+end
