b0b00eb0655f58d3329f51966d9c15340783d49a
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2010-12-10 16:59:32 +0100

    Added a warning report process in YamlExport#export_fachinfos

--- src/plugin/yaml.rb
+++ src/plugin/yaml.rb
@@ -4,6 +4,8 @@
 require 'plugin/plugin'
 require 'drb'
 
+require 'util/log'
+
 module ODDB 
 	class YamlExporter < Plugin
 		EXPORT_SERVER = DRbObject.new(nil, EXPORT_URI)
@@ -22,7 +24,42 @@ module ODDB
 			export_array(name, @app.doctors.values)
 		end
 		def export_fachinfos(name='fachinfo.yaml')
-			export_array(name, @app.fachinfos.values)
+          # Check missing data of fachinfo data
+          no_descr = {'de' => [], 'fr' => []}
+          @app.fachinfos.values.each do |fachinfo|
+            swissmedic_registration_numbers = ODBA.cache.fetch(fachinfo.odba_id, nil).iksnrs
+            no_descr.keys.each do |language|
+              unless fachinfo.descriptions[language]
+               no_descr[language].concat swissmedic_registration_numbers
+              end
+            end
+          end
+
+          # Send a warning report of fachinfo description
+          if no_descr.values.flatten.length > 0
+            log = Log.new(@@today)
+            message = []
+            no_descr.keys.each do |language|
+              unless no_descr[language].empty?
+                message.concat([
+                  "There is no '#{language}' description of Fachinformation of the following",
+                  "Swissmedic Registration Number:",
+                  no_descr[language].join(", ").to_s
+                ])
+              end
+            end
+            log.report = [
+              "Message: ",
+              "YamlExporter#export_fachinfs method is still running,",
+              "but I found some missing Fachinfo document data.",
+              "This may cause an error in export ebooks process of ebps.",
+              "",
+            ].concat(message).join("\n")
+            log.notify(" Warning Export: #{name}")
+          end
+
+          # Go to exporting process
+          export_array(name, @app.fachinfos.values)
 		end
     def export_interactions(name='interactions.yaml')
       export_array(name, @app.substances.inject([]) { |memo, sub| memo.concat sub.substrate_connections.values })
