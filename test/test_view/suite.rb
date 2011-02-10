e8100117f8f3ac104c07f1297e18417195864d10
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-02-10 07:40:17 +0100

    Changed tests order of test_view because of interdependency of test-cases

--- test/test_view/suite.rb
+++ test/test_view/suite.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
-# View::OneDirSuite -- oddb -- 20.10.2003 -- mhuggler@ywesee.com
+# View::OneDirSuite -- oddb -- 10.02.2011 -- mhatakeyama@ywesee.com
 
 $: << File.expand_path(File.dirname(__FILE__))
 
-Dir.foreach(File.dirname(__FILE__)) { |file|
-	if /.*\.rb$/o.match(file)&&file!='suite.rb'
-		require file 
-	end
-}
+Dir.open(File.dirname(__FILE__)) do |dir|
+  dir.sort.each { |file|
+	  if /.*\.rb$/o.match(file)&&file!='suite.rb'
+		  require file 
+	  end
+  }
+end
