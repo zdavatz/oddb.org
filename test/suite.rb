11f66c435d6ba92229494ba932fb3abbc1c27c13
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-02-10 17:03:56 +0100

    Updated test/suite.rb in order to execute each suite.rb independently

--- test/suite.rb
+++ test/suite.rb
@@ -3,6 +3,8 @@
 
 $: << File.dirname(__FILE__)
 
+require 'tempfile'
+
 directories = []
 
 Dir.foreach(File.dirname(__FILE__)) { |dir|
@@ -11,13 +13,85 @@ Dir.foreach(File.dirname(__FILE__)) { |dir|
 	end
 }
 
-directories.each { |dir|
+rcov = true
+coverage = nil
+#command = 'system "rcov #{path} -t --aggregate #{coverage.path} >> #{temp_out.path 2>/dev/null}"'
+command = 'system "rcov #{path} -t --aggregate #{coverage.path} >> #{temp_out.path}"'
+begin
+  Rcov
+  coverage = Tempfile.new('coverage')
+rescue
+  rcov = false
+  command = 'system "ruby #{path} >> #{temp_out.path}"'
+end
+
+temp_out = Tempfile.new('temp_out')
+directories.each_with_index { |dir, i|
 	if(File.ftype(dir) == 'directory')
-		puts dir
 		Dir.foreach(dir) { |file|
-			if /.*\.rb$/o.match(file) && file!='suite.rb'
-				require(File.expand_path(file, dir))
+			if /suite.rb$/o.match(file)
+				path = File.expand_path(file, dir)
+        puts "\nNow testing #{path}\n"
+        eval(command)
 			end
 		}
 	end
 }
+
+# report output
+test_names = []
+test_time  = 0.0
+tests      = 0
+assertions = 0
+failures   = 0
+errors     = 0
+
+failure_error = []
+failure_error_flag = false
+failure_error_no = 0
+
+rcov = nil
+temp_out.each do |line|
+  if line =~ /Loaded suite (.+)/
+    test_names << $1
+  end
+  if line =~ /Finished in ([0-9.]+) seconds/
+    test_time += $1.to_f
+  end
+  if failure_error_flag 
+    failure_error << line
+  end
+  if line =~ /(\d+)\) Failure:/ or line =~ /(\d+)\) Error:/
+    failure_error_flag = true
+    failure_error_no += 1
+    failure_error << line.gsub(/\d+/,failure_error_no.to_s)
+  end
+  if line.strip == ''
+    failure_error_flag = false
+  end
+  if line =~ /(\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors/
+    tests      += $1.to_i
+    assertions += $2.to_i
+    failures   += $3.to_i
+    errors     += $4.to_i
+  end
+  if line =~ /([0-9.]+)\%   (\d+) file\(s\)   (\d+) Lines   (\d+) LOC/
+    rcov = line
+  end
+end
+
+# result
+print <<-EOF
+
+Loaded suite 
+ #{test_names.join(",\n ")}
+
+Finished in #{test_time} seconds.
+
+#{rcov if rcov}
+#{failure_error}
+#{tests} tests, #{assertions} assertions, #{failures} failures, #{errors} errors
+EOF
+
+temp_out.close
+coverage.close if coverage
