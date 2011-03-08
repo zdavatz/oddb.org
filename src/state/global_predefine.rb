af0545e2c7f9dfb54fd45112be850158ca4335c7
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-03-08 08:31:18 +0100

    Made the code Hierarchy more readable.

--- src/state/global_predefine.rb
+++ src/state/global_predefine.rb
@@ -1,49 +1,49 @@
 #!/usr/bin/env ruby
+# State::GlobalPredefine -- oddb -- 08.03.2011 -- mhatakeyama@ywesee.com
 # State::GlobalPredefine -- oddb -- 25.11.2002 -- hwyss@ywesee.com 
 
 require 'sbsm/state'
 
 module ODDB
-	module State
-		class Global < SBSM::State
-		end
-		module Analysis
-class Global < State::Global; end
-		end
+  module State
+    class Global < SBSM::State; end
+    module Analysis
+      class Global < State::Global; end
+    end
     module Limit; end
-		module Admin 
-class Global < State::Global; end
-module Root; end
-module Admin; end
-module CompanyUser; end
-module PowerUser; end
-module PowerLinkUser; end
-module LoginMethods; end
-		end
-		module Companies
-class Global < State::Global; end
-		end
-		module Drugs
-class Global < State::Global; end
-class Init < State::Drugs::Global; end
-		end
-		module Interactions 
-class Global < State::Global; end
-		end
-		module Substances 
-class Global < State::Global; end
-		end
-		module User 
-class Global < State::Global; end
-		end
-		module Doctors
-class Global < State::Global; end
-		end
-		module Hospitals
-class Global < State::Global; end
-		end
-		module Migel
-class Global < State::Global; end 
-		end
-	end
+    module Admin 
+      class Global < State::Global; end
+      module Root; end
+      module Admin; end
+      module CompanyUser; end
+      module PowerUser; end
+      module PowerLinkUser; end
+      module LoginMethods; end
+    end
+    module Companies
+      class Global < State::Global; end
+    end
+    module Drugs
+      class Global < State::Global; end
+      class Init < State::Drugs::Global; end
+    end
+    module Interactions 
+      class Global < State::Global; end
+    end
+    module Substances 
+      class Global < State::Global; end
+    end
+    module User 
+      class Global < State::Global; end
+    end
+    module Doctors
+      class Global < State::Global; end
+    end
+    module Hospitals
+      class Global < State::Global; end
+    end
+    module Migel
+      class Global < State::Global; end 
+    end
+  end
 end
--- src/state/migel/product.rb
+++ src/state/migel/product.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/env ruby
+# State::Migel::Product -- oddb -- 08.03.2011 -- mhatakeyama@ywesee.com
 # State::Migel::Product -- oddb -- 05.10.2005 -- ffricker@ywesee.com
 
 require 'view/migel/product'
 
 module ODDB
-	module State
-		module Migel
-class Product < Global
-	VIEW = View::Migel::Product
-	LIMITED = true
-end
-		end
-	end
+  module State
+    module Migel
+      class Product < Global
+        VIEW = View::Migel::Product
+        LIMITED = true
+      end
+    end
+  end
 end
