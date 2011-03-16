079a929f591236d5ab317ea95ca5947c2121d16e
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-03-16 16:46:36 +0100

    Updated test-cases, test_view/admin/package.rb, test_plugin/narcotic.rb

--- test/test_plugin/narcotic.rb
+++ test/test_plugin/narcotic.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
+# ODDB::TestNarcoticPlugin -- oddb -- 16.03.2011 -- mhatakeyama@ywesee.com
 # ODDB::TestNarcoticPlugin -- oddb -- 03.11.2005 -- ffricker@ywesee.com
 
-$: << File.expand_path('../../src', 
-File.dirname(__FILE__))
+$: << File.expand_path('../../src', File.dirname(__FILE__))
 $: << File.expand_path('..', File.dirname(__FILE__))
 
 
 require 'test/unit'
 require 'plugin/narcotic'
 require 'flexmock'
+require 'mechanize'
 
 module ODDB
 	class TestNarcoticPlugin < Test::Unit::TestCase
+    include FlexMock::TestCase
 		def setup
-			@app = FlexMock.new
+			@app = flexmock('app')
 			@plugin = NarcoticPlugin.new(@app)
 		end
 		def test_casrns
@@ -55,6 +57,26 @@ module ODDB
       expected = { "Dextropropoxyphen" => "Dextropropoxyphenhaltige" }
       assert_equal expected, @plugin.instance_variable_get('@narcotic_texts')
 		end
+    def test_update_narcotic__else
+      narcotic = flexmock('narcotic', :pointer => 'pointer')
+      flexmock(@app, 
+               :update => 'update',
+               :narcotic_by_casrn => narcotic
+              )
+      row = [0,1,2,3,4,'d']
+      assert_equal('update', @plugin.update_narcotic(row, 'casrn', :de))
+    end
+    def test_update_narcotic__no_narcotic
+      narcotic = flexmock('narcotic')
+      flexmock(@app, 
+               :update => 'update',
+               :narcotic_by_casrn => nil,
+               :narcotic_by_smcd  => nil
+              )
+      row = [0,1,2,3,4,'d']
+      assert_equal('update', @plugin.update_narcotic(row, 'casrn', :de))
+    end
+
 		def test_narcotic_text
 			text = "Codeinhaltige"
 			assert_equal("Codein", @plugin.text2name(text, :de))
@@ -73,5 +95,184 @@ module ODDB
 			row = ['Codein-Oxid-H2O', 'casrn', 'pcode', '7680543210079']
 			assert_equal('Codein-Oxid-H2O', @plugin.strip_name(row).at(0))
 		end
+    def test_update_package
+      package = flexmock('package', 
+                         :odba_store   => nil,
+                         :add_narcotic => nil,
+                         :pointer      => 'pointer'
+                        )
+      registration = flexmock('registration', :package => package)
+      flexmock(@app, :registration => registration)
+      narcs = {'123' => 'narc'}
+      @plugin.instance_eval('@narcs = narcs')
+      row = [0,'123',2,'7680123456789']
+      assert_equal(nil, @plugin.update_package(row, 'language'))
+    end
+    def test_update_package__unknown_package
+      flexmock(Package, :find_by_pharmacode => nil)
+      registration = flexmock('registration', :package => nil)
+      flexmock(@app, :registration => registration)
+      narcs = {'123' => 'narc'}
+      @plugin.instance_eval('@narcs = narcs')
+
+      row = ['0','123', 'pcode', '7680123456789']
+      expected = ["0\n123 | pcode | 7680123456789\n"]
+      assert_equal(expected, @plugin.update_package(row, 'language'))
+    end
+    def test_update_package__unknown_registration
+      flexmock(@app, :registration => nil)
+      row = ['0']
+      assert_equal(["0\n\n"], @plugin.update_package(row, 'language'))
+    end
+    def test_update_substance
+      substance = flexmock('substance', :pointer => 'pointer')
+      flexmock(@app, 
+               :substance_by_smcd => substance,
+               :update => nil
+              )
+      row = ['name ( voir )',1,2,'7680123456789']
+      assert_equal(nil, @plugin.update_substance(row, 'casrn', 'narc', 'language'))
+    end
+    def test_update_substance__else
+      substance = flexmock('substance', :pointer => 'pointer')
+      flexmock(@app, 
+               :update    => substance,
+               :substance => nil,
+               :substance_by_smcd => nil
+              )
+      row = ['name ( voir )',1,2,'7680123456789']
+      assert_equal(substance, @plugin.update_substance(row, 'casrn', 'narc', 'language'))
+    end
+    def test_update_narcotic_texts
+      flexmock(@app, :update => nil)
+      narcotic_texts = {'name' => 'text'}
+      @plugin.instance_eval('@narcotic_texts = narcotic_texts')
+      pointer = flexmock('pointer', :creator => nil)
+      flexmock(pointer, :+ => pointer)
+      narcotic = flexmock('narcotic', :pointer => pointer)
+      substance = flexmock('substance', 
+                           :language => 'name',
+                           :narcotic => narcotic
+                          )
+      reserve_substances = [substance]
+      @plugin.instance_eval('@reserve_substances = reserve_substances')
+      assert_equal({"name"=>"text"}, @plugin.update_narcotic_texts('language'))
+    end
+    def test_report
+      expected = "Narcotics: 0\nNarcotics with CASRN: 0\nPackages with Narcotics: 0\nUnknown registrations: 0\nUnknown packages: 0\nCreated Substances: 0\nCreated Narcotics: 0\nCreated Narcotic Texts: 0\nRemoved Narcotics 0\n\n\nName\nCasrn | Pharmacode | Ean-Code | Company | Level\n\n\nUnknown registrations: 0\n\n\n\n\nUnknown packages: 0\nPackungen, die weder anhand des Swissmedic-Codes noch anhand des\nPharmacodes in der ODDB gefunden wurden. Kann auch ausser-Handel\nPackungen beinhalten.\nDiese Produkte werden in ch.oddb.org nicht angezeigt (zu wenig Informationen).\n\n\n\n\nNew substances: 0\n\n\n\n\nNew Narcotic Texts: 0\n\n\n\n\nRemoved Narcotics 0\n\n"
+      assert_equal(expected, @plugin.report)
+    end
+    def test_report_text
+      row = [nil, 'row', 'row']
+      expected = " | row | row\n"
+      assert_equal(expected, @plugin.report_text(row))
+    end
+    def test_text2name__error
+      assert_raise(RuntimeError) do 
+        @plugin.text2name('text', :jp)
+      end
+    end
+    def test_prune_narcotics
+      narcotic = flexmock('narcotic', 
+                          :casrn           => 'casrn',
+                          :category        => 'category'
+                         )
+      package  = flexmock('package', 
+                          :narcotics       => [narcotic],
+                          :pointer         => 0,
+                          :include?        => nil,
+                          :remove_narcotic => nil,
+                          :name_base       => 'name_base',
+                          :pharmacode      => 'pharmacode',
+                          :barcode         => 'barcode',
+                          :company_name    => 'company_name'
+                         )
+      @app.should_receive(:each_package).and_yield(package)
+      updated_packages = [package]
+      @plugin.instance_eval('@updated_packages = updated_packages')
+      assert_equal(nil, @plugin.prune_narcotics)
+    end
+    def test_prune_narcotics__narcs_not_empty
+      narcotic = flexmock('narcotic', 
+                          :casrn           => 'casrn',
+                          :category        => 'category',
+                          :pointer         => nil
+                         )
+      package  = flexmock('package', 
+                          :narcotics       => [narcotic],
+                          :pointer         => 0,
+                          :include?        => nil,
+                          :remove_narcotic => nil,
+                          :name_base       => 'name_base',
+                          :pharmacode      => 'pharmacode',
+                          :barcode         => 'barcode',
+                          :company_name    => 'company_name'
+                         )
+      flexmock(@app) do |a|
+        a.should_receive(:each_package).and_yield(package)
+        a.should_receive(:narcotics).and_return({'oid' => narcotic})
+        a.should_receive(:delete)
+      end
+      updated_packages = [package]
+      @plugin.instance_eval('@updated_packages = updated_packages')
+      narcs = ['narcs']
+      @plugin.instance_eval('@narcs = narcs')
+      expected = {"oid" => narcotic}
+      assert_equal(expected, @plugin.prune_narcotics)
+    end
+    def test_process_row
+      row = [0,1,2,'7680123456789']
+      expected = [[0, 1, 2, "7680123456789"]]
+      assert_equal(expected, @plugin.process_row(row, :de))
+    end
+    def test_process_row__not_switzerland
+      narcotic  = flexmock('narcotic', :pointer => 'pointer')
+      update    = flexmock('update', :oid => 'oid')
+      substance = flexmock('substance', :pointer => 'pointer')
+      flexmock(@app, 
+               :update => update,
+               :narcotic_by_casrn => narcotic,
+               :substance_by_smcd => substance
+              )
+      row = [0,'1/2',2,'7611123456789']
+      assert_equal(update, @plugin.process_row(row, :de))
+    end
+
 	end
 end
+
+class TestNarcoticHandler < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    callback = Proc.new{'callback'}
+    @handler = ODDB::NarcoticHandler.new(callback)
+  end
+  def test_send_page
+    line = ['name', 'casrn', 'third']
+    @handler.instance_eval('@lines[0] = line')
+    expected = [["name", nil, nil, nil, "casrn", nil]]
+    assert_equal(expected, @handler.send_page)
+  end
+  def test_send_page__casrn_empty
+    line = [' 1-2-3 ', '', 'third']
+    @handler.instance_eval('@lines[0] = line')
+    assert_equal([], @handler.send_page)
+  end
+  def test_send_page__match_third
+    line = ['name', 'casrn', ' 1-2-3 ']
+    @handler.instance_eval('@lines[0] = line')
+    expected = [["name casrn", "1-2-3", nil, nil, nil, nil]]
+    assert_equal(expected, @handler.send_page)
+  end
+  def test_send_page__compact_size_1
+    line = ['name', 'casrn', 'third']
+    @handler.instance_eval('@lines[0] = line')
+    @handler.instance_eval('@lines[1] = line')
+    expected = [
+        ["name", nil, nil, nil, "casrn casrn", nil],
+        ["name", nil, nil, nil, "casrn", nil]
+    ]
+    assert_equal(expected, @handler.send_page)
+  end
+end
+
---
+++ test/test_view/admin/package.rb
@@ -0,0 +1,244 @@
+#!/usr/bin/env ruby
+# View::Admin::TestPackage -- oddb.org -- 16.03.2011 -- mhatakeyama@ywesee.com
+
+$: << File.expand_path('../..', File.dirname(__FILE__))
+$: << File.expand_path("../../../src", File.dirname(__FILE__))
+
+require 'test/unit'
+require 'flexmock'
+require 'htmlgrid/select'
+require 'view/admin/package'
+require 'htmlgrid/textarea'
+
+class TestCompositionSelect < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    @session = flexmock('session')
+    @model   = flexmock('model')
+    @select  = ODDB::View::Admin::CompositionSelect.new('name', @model, @session)
+  end
+  def test_selection
+    composition  = flexmock('composition')
+    registration = flexmock('registration', :compositions => [composition])
+    flexmock(@model,   
+             :composition   => composition,
+             :registration  => registration
+            )
+    flexmock(@session, :language => 'language')
+
+    context = flexmock('context', :option => 'option')
+    assert_equal(['option'], @select.selection(context))
+  end
+  def test_shorten
+    expected = 'c'*57 + '...'
+    assert_equal(expected, @select.shorten('c'*70))
+  end
+end
+
+class TestParts < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    @lookandfeel = flexmock('lookandfeel',
+                            :lookup     => 'lookup',
+                            :attributes => {},
+                            :event_url  => 'event_url'
+                           )
+    @model = flexmock('model',
+                     :multi => 'multi',
+                     :count => 'count',
+                     :commercial_form => 'commercial_form',
+                     :measure => 'measure',
+                     :pointer => 'pointer'
+                     )
+    state = flexmock('state', :model => @model)
+    @session = flexmock('session',
+                        :lookandfeel => @lookandfeel,
+                        :state => state
+                       )
+    @parts = ODDB::View::Admin::Parts.new([@model], @session)
+  end
+  def test_input_text
+    flexmock(@model, :key => 'key')
+    keys = [:key]
+    ODDB::View::Admin::Parts.input_text(*keys)
+    assert_kind_of(HtmlGrid::Input, @parts.key(@model))
+  end
+  def test_delete
+    parts = ODDB::View::Admin::Parts.new([@model, @model], @session)
+    assert_kind_of(HtmlGrid::Link, parts.delete(@model))
+  end
+end
+
+class TestPackageForm < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    @lookandfeel = flexmock('lookandfeel', 
+                            :lookup       => 'lookup',
+                            :attributes   => {},
+                            :format_price => 'format_price',
+                            :_event_url   => '_event_url'
+                           )
+    @session = flexmock('session', 
+                        :lookandfeel => @lookandfeel,
+                        :error       => 'error',
+                        :warning?    => nil,
+                        :error?     => nil
+                       )
+    sl_entry = flexmock('sl_entry', :pointer => 'pointer')
+    package = flexmock('package',
+                        :generic_group_factor => 2.5,
+                        :ikskey => 'ikskey'
+                      )
+    @model   = flexmock('model', 
+                        :out_of_trade => 'out_of_trade',
+                        :generic_group_factor => 1,
+                        :sl_entry => sl_entry,
+                        :generic_group_comparables => [package]
+                       )
+    @form = ODDB::View::Admin::PackageForm.new(@model, @session)
+  end
+  def test_init
+    assert_equal(nil, @form.init)
+  end
+  def test_sl_entry
+    flexmock(@model, :sl_entry => nil)
+    flexmock(@lookandfeel, :event_url => nil)
+    assert_kind_of(HtmlGrid::Link, @form.sl_entry(@model, @session))
+  end
+  def test_generic_group
+    assert_kind_of(HtmlGrid::Textarea, @form.generic_group(@model, @session))
+  end
+end
+
+class TestPackageComposite < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    @lookandfeel = flexmock('lookandfeel', 
+                            :disabled?  => nil,
+                            :enabled?   => nil,
+                            :attributes => {},
+                            :lookup     => 'lookup'
+                           )
+    @app     = flexmock('app')
+    @session = flexmock('session', 
+                        :app         => @app,
+                        :lookandfeel => @lookandfeel,
+                        :error       => 'error'
+                       )
+    parent   = flexmock('parent', :name => 'name')
+    @model   = flexmock('model',
+                        :parent => parent,
+                        :size   => 'size',
+                        :price_exfactory => 'price_exfactory',
+                        :price_public    => 'price_public'
+                       )
+    @composite = ODDB::View::Admin::PackageComposite.new(@model, @session)
+  end
+  def test_package_name
+    expected = 'name&nbsp;-&nbsp;size'
+    assert_equal(expected, @composite.package_name(@model, @session))
+  end
+  def test_source
+    flexmock(@model, :swissmedic_source => 'swissmedic_source')
+    assert_kind_of(HtmlGrid::Value, @composite.source(@model, @session))
+  end
+end
+
+class TestRootPackageComposite < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def setup
+    @lookandfeel = flexmock('lookandfeel',
+                            :attributes   => {},
+                            :lookup       => 'lookup',
+                            :format_price => 'format_price',
+                            :event_url    => 'event_url',
+                            :_event_url   => '_event_url',
+                            :base_url     => 'base_url'
+                           )
+    @app     = flexmock('app')
+    state    = flexmock('state')
+    @session = flexmock('session', 
+                        :app         => @app,
+                        :lookandfeel => @lookandfeel,
+                        :error       => 'error',
+                        :warning?    => nil,
+                        :error?      => nil,
+                        :state       => state
+                       )
+    parent   = flexmock('parent', :name => 'name')
+    package  = flexmock('package',
+                        :ikskey => 'ikskey',
+                        :generic_group_factor => 2.5
+                      )
+    sl_entry = flexmock('sl_entry', :pointer => 'pointer')
+    part     = flexmock('part', 
+                        :multi   => 'multi',
+                        :count   => 'count',
+                        :measure => 'measure',
+                        :commercial_form => 'commercial_form'
+                       )
+    @model   = flexmock('model',
+                        :parent       => parent,
+                        :size         => 'size',
+                        :out_of_trade => 'out_of_trade',
+                        :sl_entry     => sl_entry,
+                        :parts        => [part],
+                        :pointer      => 'pointer',
+                        :generic_group_factor => 1,
+                        :generic_group_comparables => [package],
+                        :swissmedic_source => 'swissmedic_source'
+                       )
+    flexmock(state, :model => @model)
+    @composite = ODDB::View::Admin::RootPackageComposite.new(@model, @session)
+  end
+  def test_init
+    expected = {
+      "ACCEPT-CHARSET"=>"UTF-8",
+      "NAME"=>"stdform",
+      "METHOD"=>"POST",
+      "ACTION"=>"base_url"
+    }
+    assert_equal(expected, @composite.init)
+  end
+end
+
+class TestDeductiblePackageComposite < Test::Unit::TestCase
+  include FlexMock::TestCase
+  def test_source
+    @lookandfeel = flexmock('lookandfeel', 
+                            :attributes => {},
+                            :disabled?  => nil,
+                            :enabled?   => nil,
+                            :lookup     => 'lookup',
+                            :event_url  => 'event_url',
+                            :base_url   => 'base_url'
+                           )
+    @app       = flexmock('app')
+    state      = flexmock('state')
+    @session   = flexmock('session',
+                         :app   => @app,
+                         :error => nil,
+                         :state => state,
+                         :lookandfeel => @lookandfeel
+                         )
+    parent     = flexmock('parent', :name => 'name')
+    part       = flexmock('part',
+                         :multi   => 'multi',
+                         :count   => 'count',
+                         :measure => 'measure',
+                         :commercial_form => 'commercial_form'
+                         )
+    @model     = flexmock('model',
+                         :parent   => parent,
+                         :size     => 'size',
+                         :parts    => [part],
+                         :pointer  => 'pointer',
+                         :sequence => 'sequence',
+                         :price_public    => 'price_public',
+                         :price_exfactory => 'price_exfactory'
+                         )
+    flexmock(state, :model => @model)
+    @composite = ODDB::View::Admin::DeductiblePackageComposite.new(@model, @session)
+    assert_kind_of(HtmlGrid::Value, @composite.source(@model, @session))
+  end
+end
