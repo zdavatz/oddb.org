4b49dad60df22dc64707b2bdf3088124d471ea49
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-06-08 16:40:28 +0200

    Updated testcases, plugin/analysis.rb, swissreg.rb

---
+++ test/test_plugin/swissreg.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+# ODDB::TestSwissregPlugin -- oddb.org -- 31.05.2011 -- mhatakeyama@ywesee.com
+
+$: << File.expand_path("../../src", File.dirname(__FILE__))
+
+require 'test/unit'
+require 'flexmock'
+require 'drb'
+require 'plugin/swissreg'
+require 'uri'
+
+module ODDB
+  class TestSwissregPlugin < Test::Unit::TestCase
+    include FlexMock::TestCase
+    def stderr_null
+      require 'tempfile'
+      $stderr = Tempfile.open('stderr')
+      yield
+      $stderr.close
+      $stderr = STDERR
+    end
+    def replace_constant(constant, temp)
+      stderr_null do
+        keep = eval constant
+        eval "#{constant} = temp"
+        yield
+        eval "#{constant} = keep"
+      end
+    end
+    def setup
+      @app    = flexmock('app')
+      @plugin = ODDB::SwissregPlugin.new(@app)
+    end
+    def test_get_detail
+      server  = flexmock('server', :detail => 'detail')
+      replace_constant('ODDB::SwissregPlugin::SWISSREG_SERVER', server) do
+        assert_equal('detail', @plugin.get_detail('http://aaa.bbb.ccc'))
+      end
+    end
+    def test_format_data
+      data = {:iksnr => 'iksnr'}
+      expected = " -> http://www.swissreg.ch/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=\n"
+      assert_equal(expected, @plugin.format_data(data))
+    end
+    def test_report
+      expected = "Checked      0 Substances for connected Patents\nFound        0 Patents\nof which     0 had a Swissmedic-Number.\n             0 Registrations were successfully updated;\nfor these    0 Swissmedic-Numbers no Registration was found:\n\n"
+      assert_equal(expected, @plugin.report)
+    end
+    def test_update_registration
+      pointer = flexmock('pointer', :creator => 'creator')
+      flexmock(pointer, :+ => pointer)
+      registration = flexmock('registration', :pointer => pointer)
+      flexmock(@app, 
+               :registration => registration,
+               :update       => 'update'
+              )
+      assert_equal('update', @plugin.update_registration('iksnr', {}))
+    end
+    def test_update_registrations
+      server  = flexmock('server', :search => [{}])
+      flexmock(@plugin, :sleep => nil)
+      replace_constant('ODDB::SwissregPlugin::SWISSREG_SERVER', server) do
+        assert_equal([{}], @plugin.update_registrations('substance_name'))
+      end
+    end
+    def test_update_news
+      active_agent = flexmock('active_agent', :substance => 'substance')
+      sequence = flexmock('sequence', :active_agents => [active_agent])
+      registration = flexmock('registration') do |r|
+        r.should_receive(:each_sequence).and_yield(sequence)
+      end
+      pointer = flexmock('pointer', :resolve => registration)
+      log = flexmock('log', :change_flags => {pointer => 'value'})
+      log_group = flexmock('log_group', :latest => log)
+      flexmock(@app, :log_group => log_group)
+      server  = flexmock('server', :search => [{}])
+      flexmock(@plugin, :sleep => nil)
+      replace_constant('ODDB::SwissregPlugin::SWISSREG_SERVER', server) do
+        assert_equal(["substanc"], @plugin.update_news)
+      end
+    end
+  end
+end # ODDB
---
+++ test/test_plugin/zanalysis.rb
@@ -0,0 +1,247 @@
+#!/usr/bin/env ruby
+# ODDB::TestAnalysisPlugin -- oddb.org -- 07.06.2011 -- mhatakeyama@ywesee.com
+
+$: << File.expand_path("../../src", File.dirname(__FILE__))
+
+require 'test/unit'
+require 'flexmock'
+require 'plugin/analysis'
+
+module ODDB
+  class TestAnalysisPlugin < Test::Unit::TestCase
+    include FlexMock::TestCase
+    def setup
+      @app    = flexmock('app', :create => 'create')
+      @plugin = ODDB::AnalysisPlugin.new(@app)
+    end
+    def test_update_group
+      position = {:delete => 'groupcd'}
+      assert_equal('create', @plugin.update_group(position))
+    end
+    def test_position
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :pointer    => pointer,
+                            :permissions => permissions
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd'}
+      assert_equal('delete', @plugin.update_position(group, position, 'language'))
+    end
+    def test_position__delete_permissions
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :permissions => permissions,
+                            :pointer    => pointer
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd', :permissions => ['permission']}
+      assert_equal(update, @plugin.update_position(group, position, 'language'))
+    end
+    def test_position__delete_limitation
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :permissions => permissions,
+                            :pointer    => pointer
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd', :limitation => limitation_text}
+      assert_equal('delete', @plugin.update_position(group, position, 'language'))
+    end
+    def test_position__delete_footnote
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :pointer    => pointer,
+                            :permissions => permissions
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd', :footnote => footnote}
+      assert_equal('delete', @plugin.update_position(group, position, 'language'))
+    end
+    def test_position__delete_list_title
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :pointer    => pointer,
+                            :permissions => permissions
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd', :list_title => list_title}
+      assert_equal('delete', @plugin.update_position(group, position, 'language'))
+    end
+    def test_position__delete_taxnote
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :pointer    => pointer,
+                            :permissions => permissions
+                           )
+      flexmock(@app, 
+               :update => update,
+               :delete => 'delete'
+              )
+      group    = flexmock('group', :pointer => pointer)
+      position = {:position => 'poscd', :taxnote => taxnote}
+      assert_equal('delete', @plugin.update_position(group, position, 'language'))
+    end
+    def stderr_null
+      require 'tempfile'
+      $stderr = Tempfile.open('stderr')
+      yield
+      $stderr.close
+      $stderr = STDERR
+    end
+    def replace_constant(constant, temp)
+      stderr_null do
+        keep = eval constant
+        eval "#{constant} = temp"
+        yield
+        eval "#{constant} = keep"
+      end
+    end
+    def test_update_dacapo
+      pointer  = flexmock('pointer', :creator => 'creator')
+      flexmock(pointer, :+ => pointer)
+      position = flexmock('position', :pointer => pointer)
+      group    = flexmock('group', :position => position)
+      flexmock(@app, 
+               :analysis_group => group,
+               :update => 'update'
+              )
+      server = flexmock('ANALYSIS_PARSER') do |serv|
+        info = ['info']
+        serv.should_receive(:dacapo).and_yield('code', info)
+      end
+      replace_constant('ODDB::AnalysisPlugin::ANALYSIS_PARSER', server) do
+        assert_equal('update', @plugin.update_dacapo)
+      end
+    end
+    def test_position
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      limitation_text = flexmock('limitation_text', :pointer => pointer)
+      footnote   = flexmock('footnote', :pointer => pointer)
+      list_title = flexmock('list_title', :pointer => pointer)
+      taxnote    = flexmock('taxnote', :pointer => pointer)
+      permissions = flexmock('permissions', :pointer => pointer)
+      update     = flexmock('update', 
+                            :limitation_text => limitation_text,
+                            :footnote   => footnote,
+                            :list_title => list_title,
+                            :taxnote    => taxnote,
+                            :pointer    => pointer,
+                            :permissions => permissions
+                           )
+      group    = flexmock('group', :pointer => pointer)
+      flexmock(@app, 
+               :create  => group,
+               :update  => update,
+               :delete  => 'delete',
+               :recount => 'recount'
+              )
+      position = {:position => 'poscd', :delete => 'groupcd'}
+      server = flexmock('ANALYSIS_PARSER') do |serv|
+        serv.should_receive(:parse_pdf).and_return([position])
+      end
+      replace_constant('ODDB::AnalysisPlugin::ANALYSIS_PARSER', server) do
+        assert_equal('recount', @plugin.update('path', 'language'))
+      end
+    end
+    def test_position__analysis_revision
+      pointer  = flexmock('pointer', :creator => 'creator' )
+      flexmock(pointer, :+ => pointer)
+      group    = flexmock('group', :pointer => pointer)
+      flexmock(@app, 
+               :create  => group,
+               :recount => 'recount'
+              )
+      position = {:delete => 'groupcd', :analysis_revision => 'S'}
+      server = flexmock('ANALYSIS_PARSER') do |serv|
+        serv.should_receive(:parse_pdf).and_return([position])
+      end
+      replace_constant('ODDB::AnalysisPlugin::ANALYSIS_PARSER', server) do
+        assert_equal('recount', @plugin.update('path', 'language'))
+      end
+    end
+  end
+end
+
+
