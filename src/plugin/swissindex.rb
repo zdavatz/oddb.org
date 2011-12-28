a553f2d0de20902a57ce01085f829af4acb800f2
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-12-28 08:08:51 +0100

    Replaced pointer link in a swissindex report mail

--- src/plugin/swissindex.rb
+++ src/plugin/swissindex.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# ODDB::SwissindexPlugin -- oddb.org -- 26.08.2011 -- mhatakeyama@ywesee.com
+# ODDB::SwissindexPlugin -- oddb.org -- 28.12.2011 -- mhatakeyama@ywesee.com
 
 require 'util/oddbconfig'
 require 'plugin/plugin'
@@ -212,7 +212,7 @@ module ODDB
       lines.push "Check swissindex by eancode and then check if the package is out of trade (true) in ch.oddb," 
       lines.push "if so the package becomes in trade (false)"
       @out_of_trade_false_list.each do |pack|
-        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/resolve/pointer/%s", pack.barcode, pack.pointer)
+        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/reg/%s/seq/%s/pack/%s", pack.barcode, pack.iksnr, pack.seqnr, pack.ikscd)
       end
       lines.push nil
 
@@ -220,7 +220,7 @@ module ODDB
       lines.push "If there is no eancode in swissindex and the package is in trade in ch.oddb,"
       lines.push "then the package becomes out of trade (true) in ch.oddb"
       @out_of_trade_true_list.each do |pack|
-        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/resolve/pointer/%s", pack.barcode, pack.pointer)
+        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/reg/%s/seq/%s/pack/%s", pack.barcode, pack.iksnr, pack.seqnr, pack.ikscd)
       end
       lines.push nil
 
@@ -228,14 +228,14 @@ module ODDB
       lines.push "If the package does not have a pharmacode and there is a pharmacode found in swissindex,"
       lines.push "then put the pharmacode into ch.oddb"
       @update_pharmacode_list.each do |pack, pharmacode|
-        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/resolve/pointer/%s", pack.barcode, pack.pointer)
+        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/reg/%s/seq/%s/pack/%s", pack.barcode, pack.iksnr, pack.seqnr, pack.ikscd)
       end
       lines.push nil
 
       lines.push "Deleted pharmacode: #{@delete_pharmacode_list.size} packages"
       lines.push "If there is no eancode in swissindex then delete the according pharmacode in ch.oddb"
       @delete_pharmacode_list.each do |pack, pharmacode|
-        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/resolve/pointer/%s", pack.barcode, pack.pointer)
+        lines.push sprintf("%13i: http://ch.oddb.org/de/gcc/reg/%s/seq/%s/pack/%s", pack.barcode, pack.iksnr, pack.seqnr, pack.ikscd)
       end
       lines.join("\n")
     end
