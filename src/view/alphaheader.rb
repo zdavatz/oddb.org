57b69399dd6a7d987d65cefd9e548f2cfbe15c1f
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2012-02-29 14:54:36 +0100

    Fix NoMethodError undefined method interval for ODDB::State::Drugs::Result at src/view/alphaheader.rb:13

--- src/view/alphaheader.rb
+++ src/view/alphaheader.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# ODDB::View::AlphaHeader -- oddb.org -- 23.02.2012 -- mhatakeyama@ywesee.com 
+# ODDB::View::AlphaHeader -- oddb.org -- 29.02.2012 -- mhatakeyama@ywesee.com 
 # ODDB::View::AlphaHeader -- oddb.org -- 03.07.2003 -- hwyss@ywesee.com 
 
 module ODDB
@@ -10,22 +10,24 @@ module ODDB
 			def compose_header(offset=[0,0])
 				offset = super
 				divider = false
-				current_range = @session.state.interval
-				@session.state.intervals.each {|range|
-					@grid.add(divider, *offset) if divider
-					divider = @lookandfeel.lookup(:navigation_divider)	
-					link = HtmlGrid::Link.new(:range, @model, @session, self)
-					link.value = @lookandfeel.lookup(range.intern)
-					unless(range == current_range)
-						link.href = @lookandfeel._event_url(@session.direct_event, 
-							{'range' => range})
-					end
-					link.set_attribute('class', 'subheading bold')
-					@grid.add(link, *offset)
-				}
-				@grid.set_colspan(offset.at(0), offset.at(1), full_colspan)
-				@grid.add_style('subheading bold', *offset)
-				resolve_offset(offset, self::class::OFFSET_STEP)
+        if @session.state.respond_to?(:interval)
+          current_range = @session.state.interval
+          @session.state.intervals.each {|range|
+            @grid.add(divider, *offset) if divider
+            divider = @lookandfeel.lookup(:navigation_divider)	
+            link = HtmlGrid::Link.new(:range, @model, @session, self)
+            link.value = @lookandfeel.lookup(range.intern)
+            unless(range == current_range)
+              link.href = @lookandfeel._event_url(@session.direct_event, 
+                {'range' => range})
+            end
+            link.set_attribute('class', 'subheading bold')
+            @grid.add(link, *offset)
+          }
+          @grid.set_colspan(offset.at(0), offset.at(1), full_colspan)
+          @grid.add_style('subheading bold', *offset)
+          resolve_offset(offset, self::class::OFFSET_STEP)
+        end
 			end
 		end
 	end
