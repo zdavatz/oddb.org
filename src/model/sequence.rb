5b23a6d1d046db13f5c717ec6fd80a12076ec859
Yasuhiro Asaka <yasaka@ywesee.com>
2012-03-12 11:43:36 +0100

    Fixed PDF Patinfo link shown

--- src/model/sequence.rb
+++ src/model/sequence.rb
@@ -194,11 +194,10 @@ module ODDB
       }.compact.uniq
     end
     def has_patinfo?
-      (!@patinfo.nil? || !@pdf_patinfo.nil?) \
-        && @patinfo.respond_to?(:description) \
-        && !@patinfo.description.empty? \
-        && patinfo_active? \
-        && !company.disable_patinfo
+      ((!@patinfo.nil? && @patinfo.respond_to?(:descriptions) && !@patinfo.descriptions.nil? && !@patinfo.descriptions.empty?) \
+        || !@pdf_patinfo.nil?) \
+          && patinfo_active? \
+          && !company.disable_patinfo
     end
     def has_public_packages?
       if @packages.is_a?(Hash)
