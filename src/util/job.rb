da20ea005da5ad78f4aa6b06a0a1f1fde65b8ec3
Yasuhiro Asaka <yasaka@ywesee.com>
2012-08-20 15:37:52 +0900

    Added duplicate updater detection in Job module

--- src/util/job.rb
+++ src/util/job.rb
@@ -7,17 +7,37 @@ require 'etc/db_connection'
 module ODDB
   module Util
 module Job
+  LOG_ROOT = File.expand_path('log', PROJECT_ROOT)
+	PID_FILE = File.expand_path('job.pid', LOG_ROOT)
   def Job.run opts={}, &block
+    pid = Job.running_pid
+    job = File.basename($0)
     system = DRb::DRbObject.new(nil, ODDB.config.server_url)
     DRb.start_service
     begin
-      ODBA.cache.setup
-      ODBA.cache.clean_prefetched
-      DRb.install_id_conv ODBA::DRbIdConv.new
-      system.peer_cache ODBA.cache unless opts[:readonly] rescue Errno::ECONNREFUSED
-      block.call ODDB::App.new(:auxiliary => true)
+      if pid
+        puts "Process #{pid} is running"
+        # email notification
+      else
+        File.open(PID_FILE, 'w'){ |fh| fh << [Process.pid, job, Time.now].join(',') }
+        puts "#{PID_FILE} is created"
+        ODBA.cache.setup
+        ODBA.cache.clean_prefetched
+        DRb.install_id_conv ODBA::DRbIdConv.new
+        system.peer_cache ODBA.cache unless opts[:readonly] rescue Errno::ECONNREFUSED
+        block.call ODDB::App.new(:auxiliary => true)
+      end
     ensure
-      system.unpeer_cache ODBA.cache unless opts[:readonly]
+      unless pid
+        File.unlink(path)
+        puts "#{PID_FILE} is deleted"
+        system.unpeer_cache ODBA.cache unless opts[:readonly]
+      end
+    end
+  end
+  def Job.running_pid
+    if File.exists?(PID_FILE)
+      (File.read(PID_FILE) || '').split(',')[0]
     end
   end
 end
