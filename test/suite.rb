cbeb2ea7d7f2aefad56bab8a9bfe3fbed7fc7fd6
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-09-02 22:17:09 +0200

    Modified test/suite.rb,.simplecov for correct coverage

--- test/suite.rb
+++ test/suite.rb
@@ -4,35 +4,40 @@
 # suite.rb -- oddb.org -- 01.07.2011 -- mhatakeyama@ywesee.com 
 # In order to execute test/suite.rb,
 # yusd and meddatad is needed to run.
-require 'simplecov'
-SimpleCov.start
-SimpleCov.start do
-  add_filter "/test/"
-end
-
 $: << File.dirname(__FILE__)
 
 dir = File.expand_path(File.dirname(__FILE__))
 # Below test_suites contain tests that call each other. 
 # This can result in a wrong coverage summary as shown in the example of oddbapp.rb
 # Out of this reason we run test_util/suite.rb first - but this may cause other problems. Lets see.
-directories = [
-  
-  "#{dir}/../ext/suite.rb",
-  "#{dir}/test_model/suite.rb", 
-  "#{dir}/test_state/suite.rb",
-  "#{dir}/test_view/suite.rb",
-  "#{dir}/test_command/suite.rb",
-  "#{dir}/test_remote/suite.rb",
-  "#{dir}/test_custom/suite.rb",
+directories =  Hash.new
 
+  # oe next  creates additional error  Error: test_galenic_form(ODDB::TestGalenicGroup)
+directories["#{dir}/../ext/suite.rb" ] = true
+directories["#{dir}/test_state/suite.rb" ] = true # neither this one
+directories["#{dir}/test_view/suite.rb"] = true # does not crate
+directories["#{dir}/test_command/suite.rb"] = true
+directories["#{dir}/test_remote/suite.rb"] = true
+directories["#{dir}/test_custom/suite.rb"] = true
   # test_plugin skips bsv_xml csv_export"
-  "#{dir}/test_plugin/suite.rb",
+directories["#{dir}/test_plugin/suite.rb"] = true
   # test_util skips ipn oddbapp session updater
-  "#{dir}/test_util/suite.rb",
-]
+directories["#{dir}/test_model/suite.rb"] = true 
+directories["#{dir}/test_util/suite.rb"] = true
+ 
 
-directories.each_with_index { |path, i|
-  puts "\n#{Time.now}: Now testing #{path}\n"
-  require path
+directories.each { 
+  |path, res|
+  puts "\n#{Time.now}: Now testing #{path} #{res}\n"
+  cmd = "ruby -e\"require 'simplecov'; SimpleCov.command_name '#{File.basename(File.dirname(path), '.rb').sub('test_','')}'; SimpleCov.start; require '#{path}'\""
+  result = system(cmd) 
+  directories[path] = result
 }
+okay = true
+directories.each{ 
+  |path,res|
+    puts "#{path} returned #{res}"
+    okay = false unless res
+}
+puts "Overall result is #{okay}"
+exit(okay ? 0 : 1)
