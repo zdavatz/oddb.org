7695e096ca6af704640812f01137c929029cba58
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-03-19 14:00:59 +0100

    Watir tests for Sinovial, Rezept and instant search

---
+++ spec/medical_product_spec.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+# kate: space-indent on; indent-width 2; mixedindent off; indent-mode ruby;
+require 'spec_helper'
+require 'pp'
+
+describe "ch.oddb.org" do
+
+  RegExpSubstance = /Substanz\s+Stärke\s+(.+)\s+Galenische Form\s*(\w+)/m
+  before :all do
+    @idx = 0
+    @browser = Watir::Browser.new
+    waitForOddbToBeReady(@browser, OddbUrl)
+    login
+  end
+
+  before :each do
+    @browser.goto OddbUrl
+  end
+
+  after :each do
+    @idx += 1
+    createScreenshot(@browser, '_' + File.basename(__FILE__,'.rb') + '_'+@idx.to_s)
+    @browser.goto OddbUrl
+  end
+
+  it "should work as show/reg for sinovial" do
+    url = "http://#{OddbUrl}/de/gcc/show/reg/1229109224/seq/09/pack/224"
+    @browser.goto url
+    @browser.text.should match RegExpSubstance
+  end
+  
+  it "should work as show/reg for viagra" do
+    url = "http://#{OddbUrl}/de/gcc/show/reg/62949/seq/01/pack/001"
+    @browser.goto url
+    @browser.text.should match RegExpSubstance
+  end
+
+  after :all do
+    @browser.close
+  end
+ 
+end
