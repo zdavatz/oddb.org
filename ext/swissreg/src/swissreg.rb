9779d6833db5e59b69a7fee8d0211915504f7ab4
Niklaus Giger <ngiger@ywesee.com>
2014-05-20 17:51:17 +0200

    Fix looking for swissreg patents

--- ext/swissreg/src/swissreg.rb
+++ ext/swissreg/src/swissreg.rb
@@ -2,33 +2,135 @@
 # encoding: utf-8
 # Swissreg -- oddb.org -- 16.04.2013 -- yasaka@ywesee.com
 # Swissreg -- oddb.org -- 04.05.2006 -- hwyss@ywesee.com
-
-require 'session'
+require 'mechanize'
+require 'pp'
+require 'hpricot'
+require 'writer'
 
 module ODDB
   module Swissreg
-    def Swissreg.search(iksnr)
-      session = Session.new
-      session.get_result_list(iksnr).collect { |url, id, state, param|
-        res = {}
-        retries = 2
-        begin
-          sleep(1)
-          res = session.detail(url, id, state, param)
-        rescue
-          if(retries > 0)
-            retries -= 1
-            retry
-          else
-            raise
-          end
+    class Session
+      def initialize
+        @number = nil
+        @results = []
+        @errors  = Hash.new
+        @lastDetail =nil
+        @counterDetails = 0
+      end
+      # Weitere gesehene Fehler
+      BekannteFehler =
+            ['Das Datum ist ung', # ültig'
+            '500 Internal Server Error',
+            'Vereinfachte Trefferliste anzeigen',
+              'Es wurden keine Daten gefunden.',
+              'Die Suchkriterien sind teilweise unzul', # ässig',
+              'Geben Sie mindestens ein Suchkriterium ein',
+              'Die Suche wurde abgebrochen, da die maximale Suchzeit von 60 Sekunden',
+            'Erweiterte Suche',
+            ]
+      Base_uri = 'https://www.swissreg.ch'
+      Start_uri = "#{Base_uri}/srclient/faces/jsp/start.jsp"
+      HitsPerPage = 250
+      LogDir = 'log'
+
+      def writeResponse(agent, filename)
+        if defined?(RSpec) or defined?(MiniTest) or $VERBOSE
+          File.open(filename, 'w+') { |ausgabe| ausgabe.puts agent.page.body }
+        else
+          puts "Skipping writing #{filename}" if $VERBOSE
         end
-        res
-      }
-    end
-    def Swissreg.detail(path)
-      session = Session.new
-      session.get_detail(path)
+      end
+
+      def checkErrors(body)
+        BekannteFehler.each {
+          |errMsg|
+          if body.to_s.index(errMsg)
+            $stdout.puts "Swissreg: search has error <#{errMsg}>"
+          end
+        }
+      end
+
+      def Swissreg.init_swissreg
+        @session = Session.new
+        begin
+          @agent = Mechanize.new{
+          |agent|
+              agent.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0'
+              agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+              FileUtils.makedirs(LogDir) if $VERBOSE or defined?(RSpec)
+              agent.log = Logger.new("#{LogDir}/mechanize.log") if $VERBOSE
+          }
+          @agent.get_file  Start_uri # 'https://www.swissreg.ch/srclient/faces/jsp/start.jsp'
+          @session.writeResponse(@agent, "#{LogDir}/session_expired.html")
+          @session.checkErrors(@agent.page.body)
+          @agent.page.links[3].click
+          @session.writeResponse(@agent, "#{LogDir}/homepage.html")
+          @state = @agent.page.form["javax.faces.ViewState"]
+        rescue Net::HTTPInternalServerError, Mechanize::ResponseCodeError
+          puts "Net::HTTPInternalServerError oder Mechanize::ResponseCodeError gesehen.\n   #{Base_uri} hat wahrscheinlich Probleme"
+          raise
+        end
+      end
+    public
+      def Swissreg.search(iksnr) # search patent(s) for a given iksnr
+        $stdout.puts "Swissreg: search iksnr #{iksnr}"
+        Swissreg.init_swissreg
+        data = [
+          ["autoScroll", "0,0"],
+          ["id_swissreg:_link_hidden_", ""],
+          ["id_swissreg_SUBMIT", "1"],
+          ["id_swissreg:_idcl", "id_swissreg_sub_nav_ipiNavigation_item10"],
+          ["javax.faces.ViewState", @state],
+        ]
+        @agent.page.form['id_swissreg:_idcl'] = 'id_swissreg_sub_nav_ipiNavigation_item10'
+        @agent.page.forms.first.submit
+        @session.writeResponse(@agent, "#{LogDir}/esz_simple.html")
+        data = [
+          ["autoScroll", "0,0"],
+          ["id_swissreg:_link_hidden_", ""],
+          ["id_swissreg:mainContent:id_txf_basic_pat_no",""],
+          ["id_swissreg:mainContent:id_txf_spc_no",""],
+          ["id_swissreg:mainContent:id_txf_title",""],
+          ["id_swissreg_SUBMIT", "1"],
+          ["id_swissreg:_idcl", "id_swissreg_sub_nav_ipiNavigation_item10_item13"],
+          ["javax.faces.ViewState", @state],
+          ]
+        @agent.page.form['id_swissreg:_idcl'] = 'id_swissreg_sub_nav_ipiNavigation_item10_item13'
+        @agent.page.forms.first.submit
+        @session.writeResponse(@agent, "#{LogDir}/esz_extended.html")
+
+        form = @agent.page.forms.first
+        form.add_button_to_query(form.button(:value => /suchen/))
+        form.field(:name => 'id_swissreg:mainContent:id_txf_auth_no').value=iksnr
+        response = form.submit
+        @session.writeResponse(@agent, "#{LogDir}/first_results.html")
+        @session.checkErrors(@agent.page.body)
+        @session.extract_result_links(@agent.page)
+      end
+
+      def Swissreg.get_detail(url)
+        Swissreg.init_swissreg
+        response = @agent.get(Base_uri + url)
+        writer = DetailWriter.new
+        formatter = ODDB::HtmlFormatter.new(writer)
+        parser = ODDB::HtmlParser.new(formatter)
+        parser.feed(response.body)
+        writer.extract_data
+      rescue Timeout::Error
+        {}
+      end
+
+      def extract_result_links(response)
+        if response.is_a?(String)
+          body = Mechanize::Page.new(nil,{'content-type'=>'text/html'},response,nil, Mechanize.new)
+        else
+          body = response
+        end
+        detail_link = body.links.find{ |l| l.attributes[:id] && l.attributes[:id].match(/0:id_detail/)}
+        return [] unless detail_link
+        url =  "#{Base_uri}/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=#{detail_link.to_s}"
+        return [url]
+      end
     end
   end
 end
--- ext/swissreg/test/test_session.rb
+++ ext/swissreg/test/test_session.rb
@@ -6,46 +6,52 @@ $: << File.expand_path('../../../src', File.dirname(__FILE__))
 
 gem 'minitest'
 require 'minitest/autorun'
-require 'session'
+require 'swissreg'
 
 module ODDB
-	module Swissreg
-		class TestSession <Minitest::Test
-			def setup
-				@session = Session.new
-			end
-      def test_get_result_list__online
-        links = @session.get_result_list("sildenafil")
-				expected = [
-          "/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=C00463756/01",
-				]
-				assert_equal(expected, links)
+  module Swissreg
+    class TestSession <Minitest::Test
+      def setup
+        @session = Session.new
+        @base    = 'https://www.swissreg.ch'
+        @detail  = '/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=C00463756/01'
       end
-      def test_get_detail__online
-        url = "/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=C00463756/01"
-        data = @session.get_detail(url)
-				expected = {
-					:base_patent				=> "EP00463756",
-					:base_patent_date		=> Date.new(1991, 6, 7),
-					:certificate_number	=> "C00463756/01",
-					:expiry_date				=> Date.new(2013, 6, 21),
-					:iksnrs							=> ["54642"],
-					:issue_date					=> Date.new(1999, 7, 30),
-					:protection_date		=> Date.new(2011, 6, 7),
-					:publication_date		=> Date.new(1998, 8, 31),
-					:registration_date	=> Date.new(1998, 8, 11),
-				}
+      def test_get_result_list__online
+        links = Swissreg.search("54642")
+        expected = [
+          @base + @detail,
+        ]
+        assert_equal(expected, links)
+      end
+
+      # test result for 54642 sildenafil EP00463756
+      def test_detail_online
+        data = Swissreg.get_detail(@detail)
+        expected = {
+                :base_patent            => "EP00463756",
+                :base_patent_date       => Date.new(1991, 6, 7),
+                :certificate_number     => "C00463756/01",
+                :expiry_date            => Date.new(2013, 6, 21),
+                :iksnrs                 => ["54642"],
+                :issue_date             => Date.new(1999, 7, 30),
+                :protection_date        => Date.new(2011, 6, 7),
+                :publication_date       => Date.new(1998, 8, 31),
+                :registration_date      => Date.new(1998, 8, 11),
+                :deletion_date          => Date.new(2013, 6, 21),
+        }
         assert_equal(expected, data)
       end
-			def test_extract_result_links
-        path = File.expand_path('data/sildenafil.html', 
-                                File.dirname(__FILE__))
+
+      def test_extract_result_links
+        path = File.expand_path('data/sildenafil.html',
+        File.dirname(__FILE__))
         html = File.read(path)
-				expected = [
-          "/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=C00463756/01",
-				]
-				assert_equal(expected, @session.extract_result_links(html))
-			end
-		end
-	end
+        expected = [
+          @base + @detail,
+          # "/srclient/faces/jsp/spc/sr300.jsp?language=de&section=spc&id=C00463756/01",
+        ]
+        assert_equal(expected, @session.extract_result_links(html))
+      end
+    end
+  end
 end
