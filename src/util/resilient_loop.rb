a2ce97bf9cbb31480af893af0dd3ad5d42317046
Niklaus Giger <ngiger@ywesee.com>
2014-10-28 13:28:44 +0100

    Added resilient loop

---
+++ src/util/resilient_loop.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'util/logfile'
+require 'timeout'
+
+module ODDB
+  #
+  # ResilientLoop is a helper class for running long lasting jobs like imports
+  # It has the following characterstics
+  # * Possible to restart a failed job at the failing id
+  # * Retry an import after a timeout (e.g. of 10 seconds) 
+  #
+  # -------------
+  # requirements:
+  # -------------
+  # Needed is state-id (e.g. an EAN13 code) which allows to distingish whether
+  # a loop item was already processed or not
+  #
+  # ---------------
+  # implementation:
+  # ---------------
+  # the state is saved in a text file
+  # 
+  ExampleUsage = %(
+      r_loop = ResilientLoop.new(LoopName)
+      loop_entries.each{
+        |entry|
+          next if r_loop.must_skip?(entry)
+          r_loop.try_run(entry, TimeoutValue) { /* do your work */ }
+      }
+      r_loop.finished
+)
+  class ResilientLoop
+    attr_reader :state_file, :nr_skipped, :state_id
+    attr_writer :nr_retries
+    
+    def initialize(loopname, state_id = nil)
+      @loopname   = loopname
+      @state_id   = state_id
+      @nr_skipped = 0
+      @nr_retries = 3
+      get_state
+    end
+
+    def must_skip?(id)
+      return false unless @state_id
+      if id 
+        clear_state if id == @state_id
+        @nr_skipped += 1        
+        return true
+      else
+        return false
+      end
+    end
+
+    def try_run(state, timeout_in_secs = 10, &block)
+      idx = 0
+      while idx < @nr_retries
+        idx += 1
+        begin
+          status = timeout(timeout_in_secs) do
+            block.call
+            save_state(state)
+            return
+          end
+        rescue Timeout::Error
+          raise Timeout::Error if @nr_retries == idx
+        end
+      end
+    end
+
+    def finished
+      clear_state
+    end
+private 
+    def get_state
+      @state_file = File.join(ODDB::LogFile::LOG_ROOT, @loopname + '.state')
+      if File.exists?(@state_file)
+        content = IO.read(@state_file)
+        eval("@state_id = #{content}")
+      else
+        @state_id = nil
+      end
+    end
+    def save_state(state)
+      File.open(@state_file, 'w+') { |f| f.write(state)}
+    end
+    def clear_state
+      @state_id = nil
+      FileUtils.rm_f(@state_file)
+    end
+  end
+end
---
+++ test/test_util/resilient_loop.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+$: << File.expand_path("../../src", File.dirname(__FILE__))
+
+gem 'minitest'
+require 'minitest/autorun'
+require 'util/resilient_loop'
+
+module ODDB
+  LoopName = 'tst_loop'
+  class TestResilientLoop < Minitest::Test
+    TimeoutValue = 0.1
+    def setup
+      @r_loop = ResilientLoop.new(LoopName)
+      assert(@r_loop, 'must be able to create a ResilientLoop')
+    end
+
+    def teardown
+      @r_loop.finished
+    end
+
+    def test_full_loop_no_problem
+      assert_equal(nil, @r_loop.state_id)
+      result = []
+      loop_entries = [0,1,2,3]
+      loop_entries.each{
+        |entry|
+          @r_loop.try_run(entry, timeout_in_secs = TimeoutValue) { result << entry; }
+      }
+      @r_loop.finished
+      assert_equal(loop_entries, result)
+      assert_equal(nil, @r_loop.state_id)
+    end
+
+    def test_full_loop_always_timeout
+      assert_equal(nil, @r_loop.state_id)
+      result = []
+      loop_entries = [0,1,2,3]
+      begin
+        loop_entries.each{
+          |entry|
+            @r_loop.try_run(entry, TimeoutValue) { sleep(TimeoutValue*2); result << entry; }
+        }
+        @r_loop.finished
+      rescue Timeout::Error
+      end
+      assert_equal([], result)
+      assert_equal(nil, @r_loop.state_id)
+    end
+
+    def test_full_loop_recovers_from_first_timeout
+      assert_equal(nil, @r_loop.state_id)
+      result = []
+      loop_entries = [0,1,2,3,4,5,6]
+      toggle = false
+      begin
+        loop_entries.each{
+          |entry|
+            @r_loop.try_run(entry, TimeoutValue) {  toggle = !toggle; sleep(TimeoutValue*2) if toggle; result << entry; }
+        }
+      rescue Timeout::Error
+      end
+      assert_equal(loop_entries, result)
+      assert_equal(nil, @r_loop.state_id)
+    end
+
+    def test_skip_some_entries
+      failAtEntry = 2
+      # first simulate a loop which has a timeout at id == 2 (aka failAtEntry)
+      @r_loop = ResilientLoop.new(LoopName)
+      result = []
+      loop_entries = [0,1,2,3,4]
+      assert_raises(Timeout::Error) do
+        loop_entries.each{
+          |entry|
+            next if @r_loop.must_skip?(entry)
+            @r_loop.try_run(entry, TimeoutValue) do
+              sleep(TimeoutValue*2) if entry == failAtEntry
+              result << entry;
+            end
+        }
+      end
+      assert_equal([0,1], result)
+      assert_equal(nil, @r_loop.state_id)
+      assert_equal(0, @r_loop.nr_skipped)
+      assert(File.exist?(@r_loop.state_file))
+
+      # now start with a fresh loop
+      @r_loop = ResilientLoop.new(LoopName)
+      assert(File.exist?(@r_loop.state_file))
+      assert_equal(1, @r_loop.state_id)
+      result = []
+      loop_entries.each{
+        |entry|
+          next if @r_loop.must_skip?(entry)
+          @r_loop.try_run(entry, TimeoutValue) { result << entry; }
+      }
+      assert_equal(nil, @r_loop.state_id)
+      assert_equal([2,3,4], result)
+      assert_equal(2, @r_loop.nr_skipped)
+    end
+  end
+end
+
