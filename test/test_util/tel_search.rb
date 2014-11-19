2b30c18e2a59cbdebb9f815fd4e4af50d139b045
Niklaus Giger <ngiger@ywesee.com>
2014-11-19 16:35:56 +0100

    Add tel.search

---
+++ test/test_util/tel_search.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+$: << File.expand_path('..', File.dirname(__FILE__))
+$: << File.expand_path("../../src", File.dirname(__FILE__))
+
+gem 'minitest'
+require 'minitest/autorun'
+require 'util/tel_search'
+
+class TestTelSearch <Minitest::Test
+  def test_niklaus_gigerwhich_has_no_fax_published
+    assert_equal('055 612 20 54', TelSearch.search('niklaus giger', 8753, 'Wieshoschet'))
+    assert_equal(nil, TelSearch.search('niklaus giger', 8753, 'Wieshoschet', :fax))
+  end
+  def test_daniel_pfister_which_has_a_fax
+    assert_equal('055 612 22 22', TelSearch.search('Daniel Pfister', 8753))
+    assert_equal('055 612 01 47', TelSearch.search('Daniel Pfister', 8753, nil, :fax))
+  end
+  def return_nil_if_too_many_result
+    assert_equal(nil, TelSearch.search('giger'))
+  end
+  def test_raises_if_wrong_phone_type_given
+    assert_raises(RuntimeError) do
+      TelSearch.search('niklaus giger', 8753, 'Wieshoschet', :testing_invalid_type)
+    end
+  end
+end
