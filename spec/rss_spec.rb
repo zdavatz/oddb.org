2acde1d3ddfabd66859c7492ed599acf10a83809
Niklaus Giger <ngiger@ywesee.com>
2015-11-30 10:57:57 +0100

    Made rss-test pass with firefox by increasing the timeout

--- spec/rss_spec.rb
+++ spec/rss_spec.rb
@@ -8,10 +8,12 @@ describe "ch.oddb.org" do
  
   before :all do
     @idx = 0
+    @saved_timeout ||= Watir.default_timeout
     waitForOddbToBeReady(@browser, OddbUrl)
   end
-  
+
   before :each do
+    Watir.default_timeout = @saved_timeout
     @browser.goto OddbUrl
   end
 
@@ -35,12 +37,15 @@ describe "ch.oddb.org" do
         @browser.goto OddbUrl
         expect(@browser.url).to match OddbUrl
         link = @browser.link(:href => /rss\/channel\/#{subject}/)
-        debug = true
+        debug = /fachinfo/i.match(subject) ? true : false
         expect(link.exists?).to be true
         if @browser.driver.browser.eql?(:firefox)
+          Watir.default_timeout = 60 if /fachinfo/i.match(subject) # reading fachinfo takes a long time
+          $stdout.puts "#{Time.now}: goto #{link.href} timeout #{Watir.default_timeout}" if debug
           @browser.goto(link.href)
           # We have problem as the fachinfo size is 26MB and goes back to 2006.
-          $stdout.puts "#{Time.now}: #{@browser.url} start reading text" if debug
+          Watir.default_timeout = 600 if /fachinfo/i.match(subject) # reading fachinfo takes a long time
+          $stdout.puts "#{Time.now}: #{@browser.url} start reading text timeout #{Watir.default_timeout}" if debug
           content = @browser.text.clone
           File.open(subject + '.rss', 'w+' ) { |f| f.write content }
           expect(content).to match /#{test_string}/
@@ -53,7 +58,8 @@ describe "ch.oddb.org" do
     }
 
   after :all do
-    $stdout.puts "#{Time.now}: #{__FILE__} after all"
+    Watir.default_timeout = @saved_timeout
+    $stdout.puts "#{Time.now}: #{__FILE__} after all. Restored timeout to #{@saved_timeout}"
     @browser.close
   end
 end
