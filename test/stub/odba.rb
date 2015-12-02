9559b679d515c38b3f88e56a02644e255fb1555b
Niklaus Giger <ngiger@ywesee.com>
2015-12-02 17:42:50 +0100

    Remove dummy test for retrieve_from_index

--- test/stub/odba.rb
+++ test/stub/odba.rb
@@ -12,6 +12,7 @@ PGresult = PG::Result
 
 module ODBA
 	class CacheStub
+		attr_writer :retrieve_from_index
 		def delete(anything)
 		end
     def ensure_index_deferred *args
@@ -38,8 +39,6 @@ module ODBA
       else
         []
       end
-    end
-    def create_index(index_definition, origin_module=Object)
     end
     def drop_index(index_name)
     end
