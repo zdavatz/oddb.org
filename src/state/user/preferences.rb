3a6fa6397d45c51d78ff4770dbb48b3e40f1471b
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-09-20 16:07:32 +0200

    Fix saving search_type

--- src/state/user/preferences.rb
+++ src/state/user/preferences.rb
@@ -15,9 +15,14 @@ class Preferences < State::Global
     if style = @session.user_input(:style)
       @session.set_cookie_input(:style, style)
     end
-    [ :search_type,
-      :search_form,
-      :search_limitation_A,
+    [:search_type, :search_form].each do |key|
+      if val = @session.user_input(key)
+        @session.set_cookie_input(key, val)
+        @session.set_persistent_user_input(key, val)
+        puts "1. Setting #{key} to #{val.inspect} from #{@session.user_input(key).inspect}"
+      end
+    end
+    [ :search_limitation_A,
       :search_limitation_B,
       :search_limitation_C,
       :search_limitation_D,
@@ -28,7 +33,7 @@ class Preferences < State::Global
       val = !!@session.user_input(key)
       @session.set_cookie_input(key, val.to_s)
       @session.set_persistent_user_input(key, val.to_s)
-      puts "Setting #{key} to #{val.inspect} from #{@session.user_input(key).inspect}"
+      puts "2. Setting #{key} to #{val.inspect} from #{@session.user_input(key).inspect}"
     end
     if zsr_id = @session.user_input(:zsr_id)
       zsr_id = zsr_id.gsub(/[ \.]/, '')
