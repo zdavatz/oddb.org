0e19982500ff07c9547768cadb76e0a67675c92c
Niklaus Giger <ngiger@ywesee.com>
2017-11-15 18:06:28 +0100

    Re-add TabNavigation in ResultTemplate

--- src/view/resulttemplate.rb
+++ src/view/resulttemplate.rb
@@ -6,6 +6,7 @@ require 'view/publictemplate'
 require 'view/navigation'
 require 'view/welcomehead'
 require 'view/logohead'
+require 'view/tab_navigationlink'
 
 module ODDB
 	module View
@@ -15,11 +16,17 @@ module ODDB
 			def init
 				@components = {
 					[0,0]		=>	:head,
-					[0,1]		=>	:content,
-					[0,2]		=>	:foot,
+					[0,1]		=>	:tab_navigation,
+					[0,2]		=>	:content,
+					[0,3]		=>	:foot,
 				}
 				super
 			end
+      def tab_navigation(model, session=@session)
+        unless @lookandfeel.disabled?(:search_result_head_navigation)
+          View::TabNavigation.new(model, session, self)
+        end
+      end
 		end
 	end
 end
