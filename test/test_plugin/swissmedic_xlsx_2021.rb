13d96c8353dd344308dcaf50daf2a0046404dc07
Niklaus Giger <niklaus.giger@member.fsf.org>
2025-07-11 22:04:37 +0200

    Issue #330: Fix import_swissmedic

--- test/test_plugin/swissmedic_xlsx_2021.rb
+++ test/test_plugin/swissmedic_xlsx_2021.rb
@@ -43,40 +43,30 @@ module ODDB
       ODDB::TestHelpers.vcr_setup
       mock_downloads
       @app = flexmock(ODDB::App.new)
-      @archive = ODDB::WORK_DIR
-      FileUtils.rm_rf(@archive)
-      FileUtils.mkdir_p(@archive)
-      @latest = File.join ODDB::TEST_DATA_DIR, 'xls', 'Packungen-latest.xlsx'
-      @older  = File.join(ODDB::TEST_DATA_DIR, 'xls/Packungen-2019.01.31.xlsx')
-      @plugin = flexmock('plugin', SwissmedicPlugin.new(@app, @archive))
+      FileUtils.rm_rf(ODDB::WORK_DIR, :verbose => true)
+      FileUtils.mkdir_p(File.join(ODDB::WORK_DIR, 'xls'))
+      FileUtils.mkdir_p(File.join(ODDB::WORK_DIR, 'xlsx'))
+      @latest = File.join ODDB::TEST_DATA_DIR, 'xlsx', 'Packungen-latest.xlsx'
+      @older  = File.join(ODDB::TEST_DATA_DIR, 'xlsx', 'Packungen-2019.01.31.xlsx')
+      @plugin = flexmock('plugin', SwissmedicPlugin.new(@app, ODDB::WORK_DIR))
       @bag_listen  = File.join(ODDB::TEST_DATA_DIR, 'html/listen_neu.html')
       @current  = File.join(ODDB::TEST_DATA_DIR, 'xlsx/Packungen-2021.04.01.xlsx')
-      @target   = File.join @archive, 'xls',  @@today.strftime('Packungen-%Y.%m.%d.xlsx')
+      @target   = File.join ODDB::WORK_DIR, 'xls',  @@today.strftime('Packungen-%Y.%m.%d.xlsx')
       @plugin.should_receive(:fetch_with_http).with('https://www.swissmedic.ch/swissmedic/de/home/services/listen_neu.html').and_return(File.open(@bag_listen).read).by_default
       @plugin.should_receive(:open).with( ODDB::SwissmedicPlugin.get_packages_url).and_return(File.open(@current).read).by_default
-      @prep_from = File.join(ODDB::TEST_DATA_DIR, 'xlsx/Erweiterte_Arzneimittelliste_HAM_31012019.xlsx')
-      FileUtils.cp(@prep_from, File.join(@archive, 'xls',  @@today.strftime('Erweiterte_Arzneimittelliste_HAM_31012019.xlsx')),
+      @prep_from = File.join(ODDB::TEST_DATA_DIR, 'xlsx', 'Erweiterte_Arzneimittelliste_HAM_31012019.xlsx')
+      FileUtils.cp(@prep_from, File.join(ODDB::WORK_DIR, 'xlsx','Erweiterte_Arzneimittelliste_HAM_31012019.xlsx'),
                    :verbose => true, :preserve => true)
       @plugin.should_receive(:fetch_with_http).with( ODDB::SwissmedicPlugin.get_preparations_url).and_return(File.open(@prep_from).read).by_default
       FileUtils.makedirs(File.dirname(@latest)) unless File.exist?(File.dirname(@latest))
       FileUtils.cp(@latest, File.join(ODDB::WORK_DIR, 'xls'), :verbose => true)
       FileUtils.cp(@older, File.join(ODDB::WORK_DIR, 'xls'), :verbose => true)
-
-      puts File.join(ODDB::WORK_DIR, 'xlsx')
-      puts @latest
-      assert(true, File.exist?('/opt/src/oddb.org-3.4/data4tests/xls/Packungen-latest.xlsx'))
-      require 'debug'; binding.break
-
     end
     def teardown
       ODBA.storage = nil
       super # to clean up FlexMock
     end
     def test_april_2021
-      # Use first the last month and compare it to a non existing lates
-      FileUtils.rm(Dir.glob("#{File.dirname(@latest)}/*"), :verbose => true)
-      FileUtils.cp(@older, @latest, :verbose => true, :preserve => true)
-      FileUtils.cp(@current, @target, :verbose => true, :preserve => true)
       assert_equal(0, @app.registrations.size)
       reg = @app.create_registration('00278')
       reg.pointer = Persistence::Pointer.new([:registration, '00278'])
@@ -121,7 +111,7 @@ module ODDB
         and_return(File.open(@current).read)
       @plugin.should_receive(:fetch_with_http).with(ODDB::SwissmedicPlugin.get_preparations_url).
         and_return(File.open(@prep_from).read)
-      result = @plugin.update()
+      result = @plugin.update({:check => true})
       puts @plugin.report
       assert_nil(@app.registration('66097').expiration_date) # unbegrenzt
     end
