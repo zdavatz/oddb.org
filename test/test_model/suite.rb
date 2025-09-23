4f2385de9da2d4dfaa5cc24d23dbb854a8d25cd0
Niklaus Giger <niklaus.giger@member.fsf.org>
2025-09-23 17:17:30 +0200

    Issue #373: Fix unit test for package

--- test/test_model/suite.rb
+++ test/test_model/suite.rb
@@ -3,11 +3,11 @@
 # RecursiveSuite -- oddb.org -- 09.04.2012 -- yasaka@ywesee.com
 # RecursiveSuite -- oddb.org -- 27.04.2011 -- mhatakeyama@ywesee.com
 # OneDirSuite -- oddb -- 20.10.2003 -- mhuggler@ywesee.com
-buggy = []
 $: << File.expand_path("..", File.dirname(__FILE__))
 require "stub/odba"
+must_be_run_separately = ["package.rb"]
 require File.join(File.expand_path(File.dirname(__FILE__, 2)), "helpers.rb")
-runner = OddbTestRunner.new(File.dirname(__FILE__), buggy)
+runner = OddbTestRunner.new(File.dirname(__FILE__), must_be_run_separately)
 runner.run_isolated_tests
 runner.run_normal_tests
 runner.show_results_and_exit
